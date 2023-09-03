88ff3a2b6b138f924417ed27ef385e1ca931e21e
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-09-03 13:19:40 -0700

    Time: 103 ms (85.53%) | Memory: 52.8 MB (77.74%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -3,13 +3,25 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-   let map={}
-   for(let str of strs){
-       //for each string, sort its characters alphabetically and join them back together into a new string's'
-       let s= str.split('').sort().join('')
-       //check if the map object already has a key for the sorted string 's'. if not, create an empty array for that key.
-       if(!map[s]) map[s]=[]
-       map[s].push(str)
-   }
-   return Object.values(map) //to extract the values (arrays of anagrams) from the map object and return them as an array of arrays.
+//    let map={}
+//    for(let str of strs){
+//        //for each string, sort its characters alphabetically and join them back together into a new string's'
+//        let s= str.split('').sort().join('')
+//        //check if the map object already has a key for the sorted string 's'. if not, create an empty array for that key.
+//        if(!map[s]) map[s]=[]
+//        map[s].push(str)
+//    }
+//    return Object.values(map) //to extract the values (arrays of anagrams) from the map object and return them as an array of arrays.
+
+ const obj={}
+    strs.forEach((str)=>{
+        const key=str.split("").sort().join("")
+        if(!obj[key])obj[key]=[str]
+        else obj[key].push(str);
+    })
+       return Object.values(obj);
 };
+
+
+
+
