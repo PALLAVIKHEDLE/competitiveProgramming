b42c9af6b6fa63a5c132b0edd88379c793148f73
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-09-03 13:02:53 -0700

    Time: 122 ms (48.76%) | Memory: 55.5 MB (16.74%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+   let map={}
+   for(let str of strs){
+       //for each string, sort its characters alphabetically and join them back together into a new string's'
+       let s= str.split('').sort().join('')
+       //check if the map object already has a key for the sorted string 's'. if not, create an empty array for that key.
+       if(!map[s]) map[s]=[]
+       map[s].push(str)
+   }
+   return Object.values(map) //to extract the values (arrays of anagrams) from the map object and return them as an array of arrays.
+};
