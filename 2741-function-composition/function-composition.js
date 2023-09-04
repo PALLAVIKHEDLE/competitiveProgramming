d69cee20557f0c9790695b24f7bf4383bd3e7b7d
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-09-04 14:17:42 -0700

    Time: 58 ms (87.66%) | Memory: 43.4 MB (47.99%) - LeetSync

--- 2741-function-composition/function-composition.js
+++ 2741-function-composition/function-composition.js
@@ -2,19 +2,35 @@
  * @param {Function[]} functions
  * @return {Function}
  */
-var compose = function(functions) {
-    if(functions.length==0){
-        return function(x){return x}
-    }else{
-	return function(x) {
-        var result=functions[functions.length-1](x)
-        for(var i=functions.length-2;i>=0;i--){
-         result = functions[i](result);
-      }
-      return result;
-    }
-    }
+// var compose = function(functions) {
+//     if(functions.length==0){
+//         return function(x){return x}
+//     }else{
+// 	return function(x) {
+//         var result=functions[functions.length-1](x)
+//         for(var i=functions.length-2;i>=0;i--){
+//          result = functions[i](result);
+//       }
+//       return result;
+//     }
+//     }
+// };
+
+//reduce Right method:The compose function first checks if the input array is empty, and returns a function that simply returns its input if it is. Otherwise, it uses the reduceRight method of the array to apply the functions in reverse order. reduceRight is used instead of reduce to ensure that the functions are applied from right to left.
+
+ var compose = function(functions) {
+	if (functions.length === 0) {
+    return function(x) { return x; };
+  }
+
+  return functions.reduceRight(function(prevFn, nextFn) {
+    return function(x) {
+      return nextFn(prevFn(x));
+    };
+  });
+
 };
+
 
 /**
  * const fn = compose([x => x + 1, x => 2 * x])
