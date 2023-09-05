3ce4821c2e183125ade5d2fb7cd80163e322864c
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-09-05 11:36:38 -0700

    Time: 63 ms (30.94%) | Memory: 44.7 MB (7.16%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.js
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -3,29 +3,31 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-let start=1, commonArray=[], firstElement, secondElement, noSpecialChars
-
-   for(let k=0; k<strs.length-1; k++){
-       if(commonArray.length>0){
-           firstElement=commonArray?.join("");
-           secondElement=strs[start]
-           }else{
-               firstElement=strs[0]
-               secondElement=strs[1]
-               }
-        if(commonArray.length>0)commonArray=[]
-        
-        for(let j=0; j<secondElement.length; j++){
-            if(firstElement[j]==secondElement[j] ){ 
-                commonArray.push(firstElement[j])
-            }else break          
+    let firstElement, secondElement, commonArray=[], start=0,noSpecialChars
+    if(strs.length==1) return strs[0]
+    for(let i=0;i<strs.length;i++){
+        if(commonArray.length){
+            firstElement=commonArray?.join('')
+            secondElement=strs[start]
+        }else{
+            firstElement=strs[0]
+            secondElement=strs[1]
         }
-            start++ 
-       }
+      
+    
+        if(commonArray.length>0)commonArray=[]
+     for(let j=0;j<firstElement.length;j++){
+         if(firstElement[j]==secondElement[j]){
+             commonArray.push(firstElement[j])
+         }else break
+     }
+    start++
 
-        noSpecialChars=commonArray.join("");
-            if(strs.length==1) noSpecialChars=strs[0]
+    }
+      noSpecialChars=commonArray.join("");
+   
             
             
    return noSpecialChars
+
 };
