bcca3fd4792cb463e5d44a73ee74c1873f017071
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-09-05 11:14:15 -0700

    Time: 138 ms (92.44%) | Memory: 51.4 MB (30.40%) - LeetSync

---
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var replaceElements = function(arr) {
+   let currentMax=arr[arr.length-1]
+   arr[arr.length-1]=-1
+
+   for(let i=arr.length-2; i>=0;i--){
+       if(currentMax>arr[i]) arr[i]=currentMax
+       else{
+        //    let temp=currentMax
+        //    currentMax=arr[i]
+        [currentMax, arr[i]]=[arr[i],currentMax ]
+       }
+   }
+   return arr
+};
