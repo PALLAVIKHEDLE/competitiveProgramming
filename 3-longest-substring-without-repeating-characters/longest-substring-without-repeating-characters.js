e524a58a92e062d05124463d51c85b43abb980e3
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-09-06 10:10:12 -0700

    Time: 251 ms (18.11%) | Memory: 50.4 MB (12.87%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+
+function resetMap(map,val){
+    for(key in map){
+        if(map[key]<=val) delete map[key]
+    }
+}
+
+var lengthOfLongestSubstring = function(s) {
+    let map={}, currentWindowLength, maxLength=0
+    for(let i=0; i<s.length;i++){
+        let val=map[s[i]]
+        if(val==undefined){
+            map[s[i]]=i
+            currentWindowLength=Object.keys(map).length//size of map
+            maxLength=Math.max(maxLength,currentWindowLength)
+        }else{
+            resetMap(map,val)
+            map[s[i]]=i
+        }
+    }
+  return maxLength  
+};
+
