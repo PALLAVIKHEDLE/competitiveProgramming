5ffd234641b98727ca8d186798ac5633d913cca3
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-09-07 21:09:49 -0700

    Time: 47 ms (82.98%) | Memory: 41.6 MB (71.56%) - LeetSync

--- 290-word-pattern/word-pattern.js
+++ 290-word-pattern/word-pattern.js
@@ -4,39 +4,39 @@
  * @return {boolean}
  */
 var wordPattern = function(pattern, s) {
-// let pMap = {}, sMap = {};
-// const strArray = s.split(' ');
+let pMap = Object.create(null) , sMap = Object.create(null) 
+const strArray = s.split(' ');
 
-// if (pattern.length !== strArray.length) return false;
+if (pattern.length !== strArray.length) return false;
 
-// for (let i = 0; i < pattern.length; i++) {
-//     if (!pMap[pattern[i]] && !sMap[strArray[i]]) {
-//         pMap[pattern[i]] = strArray[i];
-//         sMap[strArray[i]] = pattern[i];
-//     }else if(pMap[pattern[i]] !== strArray[i] && sMap[strArray[i]] !== pattern[i]) return false;   
-// }
+for (let i = 0; i < pattern.length; i++) {
+    if (!pMap[pattern[i]] && !sMap[strArray[i]]) {
+        pMap[pattern[i]] = strArray[i];
+        sMap[strArray[i]] = pattern[i];
+    }else if(pMap[pattern[i]] !== strArray[i] && sMap[strArray[i]] !== pattern[i]) return false;   
+}
 
-// return true;
-  const words = s.split(' '); //String to Array
+return true;
+//   const words = s.split(' '); //String to Array
   
-  if (pattern.length !== words.length) return false;
+//   if (pattern.length !== words.length) return false;
   
-  const wordToPattern = Object.create(null) 
-  const patternToWord = Object.create(null) 
+//   const wordToPattern = Object.create(null) 
+//   const patternToWord = Object.create(null) 
 
-  for (let i = 0; i < pattern.length; i++) {
-    const word = words[i];
-    const char = pattern.charAt(i);
+//   for (let i = 0; i < pattern.length; i++) {
+//     const word = words[i];
+//     const char = pattern.charAt(i);
     
-    if ((wordToPattern[word] && wordToPattern[word] !== char) ||
-        (patternToWord[char] && patternToWord[char] !== word)) {
-      return false;
-    }
+//     if ((wordToPattern[word] && wordToPattern[word] !== char) ||
+//         (patternToWord[char] && patternToWord[char] !== word)) {
+//       return false;
+//     }
     
-    wordToPattern[word] = char
-    patternToWord[char] = word
-  }
+//     wordToPattern[word] = char
+//     patternToWord[char] = word
+//   }
   
-  return true;
+//   return true;
 
 }
