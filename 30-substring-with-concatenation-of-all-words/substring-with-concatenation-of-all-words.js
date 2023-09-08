710494f2c3b452ffbca6f0087af0914a136f2c8d
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-09-08 11:38:28 -0700

    Time: 1511 ms (57.65%) | Memory: 118.3 MB (5.96%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.js
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.js
@@ -5,8 +5,7 @@
  */
 var findSubstring = function(s, words) {
     let map={}
-    let wordLength=words[0].length
-    let wordCount=words.length
+    let wordLength=words[0].length, wordCount=words.length
 
     let window=wordLength*wordCount
 
@@ -15,7 +14,6 @@ var findSubstring = function(s, words) {
         if(val==undefined) map[words[i]]=1
         else map[words[i]]=val+1
     }
-    // console.log('amp',map)
 
     let leftIndex=0, rightIndex=window-1,result=[]
 
@@ -23,18 +21,14 @@ var findSubstring = function(s, words) {
         let visited={}
         for(let j=0; j<tempStr.length;j+=wordLength){
             let word=tempStr.substr(j, wordLength)
-            // console.log('word',word)
             let val=visited[word]
             if(val==undefined)visited[word]=1
             else visited[word]=val+1
         }
-    // console.log('visited',visited)
 
-        for(key in visited){
-            console.log(map[key],visited[key],'loop')
-            if(map[key]!==visited[key]) return false
-        }
-        return true
+        for(key in visited)if(map[key]!==visited[key]) return false
+        
+    return true
     }
 
     while(rightIndex<s.length){
@@ -49,56 +43,3 @@ var findSubstring = function(s, words) {
 
   return result  
 };
-
-
-// var findSubstring = function(s, words) {
-//     let map = new Map()
-    
-//     let wordLength = words[0].length
-//     let wordCount = words.length
-    
-//     let slideWindow = wordLength * wordCount
-    
-//     for (let word of words) {
-//         map.has(word) ? map.set(word, map.get(word) + 1) : map.set(word, 1)
-//     }
-//     // console.log('map',map)
-    
-//     let leftIndex = 0
-//     let rightIndex = slideWindow - 1
-//     let result = []
-    
-//     const helper = (tempStr) => {
-//         let visited = new Map()
-        
-//         for (let i = 0; i < tempStr.length; i+= wordLength) {
-//             let word = tempStr.substr(i, wordLength)
-//             // console.log('word',word)
-//             visited.has(word) ? visited.set(word, visited.get(word) + 1) : visited.set(word, 1)
-//         }
-//         // console.log('visited',visited)
-//         for (let [key, val] of visited) {
-//             console.log(map.get(key),val,'loop')
-
-//             if (map.get(key) != val) return false
-//         }
-        
-//         return true
-//     }
-    
-//     while (rightIndex < s.length) {
-        
-//         if (rightIndex - leftIndex + 1 == slideWindow) {
-//             let tempStr = s.substring(leftIndex, rightIndex + 1)
-            
-//             if (helper(tempStr)) result.push(leftIndex)
-            
-//             leftIndex++
-//         }
-        
-        
-//         rightIndex++
-//     }
-    
-//     return result
-// };
