c42c3237fe8fe874a2764ec458b98e4a1d644ac6
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-09-07 21:08:38 -0700

    Time: 50 ms (71.91%) | Memory: 41.5 MB (87.06%) - LeetSync

--- 290-word-pattern/word-pattern.js
+++ 290-word-pattern/word-pattern.js
@@ -4,47 +4,39 @@
  * @return {boolean}
  */
 var wordPattern = function(pattern, s) {
-//     let KMap={}, VMap={}//key and value map
-//     let strArray=s.split(' ')//Convert string into array
-//     if(pattern.length!==strArray.length) return false
+// let pMap = {}, sMap = {};
+// const strArray = s.split(' ');
+
+// if (pattern.length !== strArray.length) return false;
+
+// for (let i = 0; i < pattern.length; i++) {
+//     if (!pMap[pattern[i]] && !sMap[strArray[i]]) {
+//         pMap[pattern[i]] = strArray[i];
+//         sMap[strArray[i]] = pattern[i];
+//     }else if(pMap[pattern[i]] !== strArray[i] && sMap[strArray[i]] !== pattern[i]) return false;   
+// }
+
+// return true;
+  const words = s.split(' '); //String to Array
   
-    
-//     for(let i=0;i<pattern.length;i++){
-//         if(!KMap[pattern[i]] && !VMap[strArray[i]]){
-//             KMap[pattern[i]]=strArray[i]
-//             VMap[strArray[i]]=pattern[i]
-//         }else if(KMap[pattern[i]]!==strArray[i]&&VMap[strArray[i]]!==pattern[i])return false
-//     }
-//    return true 
- const strArr = s.split(' ');
-  if (pattern.length !== strArr.length) return false;
-  const patMap = new Map();
-  const strMap = new Map();
+  if (pattern.length !== words.length) return false;
+  
+  const wordToPattern = Object.create(null) 
+  const patternToWord = Object.create(null) 
+
   for (let i = 0; i < pattern.length; i++) {
-    if (
-      (patMap.has(pattern[i]) && patMap.get(pattern[i]) !== strArr[i]) ||
-      (strMap.has(strArr[i]) && strMap.get(strArr[i]) !== pattern[i])
-    ) {
+    const word = words[i];
+    const char = pattern.charAt(i);
+    
+    if ((wordToPattern[word] && wordToPattern[word] !== char) ||
+        (patternToWord[char] && patternToWord[char] !== word)) {
       return false;
     }
-    patMap.set(pattern[i], strArr[i]);
-    strMap.set(strArr[i], pattern[i]);
-  }
-  return true;
-};
-
-
-
-//         const key=pattern[i]//letter at index i in pattern
-//         const value=s[i]//word at index i in s
     
-//     //if key or value already exists and does not corresponds to each other, return false
-//         if(key in KMap || value in VMap){
-//             if(KMap[key]!==value) return false
-//             if(VMap[value]!==key) return false
-//         }else{
-//             KMap[key]=value
-//             VMap[value]=key
-//         }
-//     }
-//  return true // pattern is valid   
+    wordToPattern[word] = char
+    patternToWord[char] = word
+  }
+  
+  return true;
+
+}
