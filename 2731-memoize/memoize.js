4e40e91f1d8fe9e53757c3339073910dc83338cc
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-09-10 12:04:30 -0700

    Time: 300 ms (55.40%) | Memory: 107.4 MB (62.51%) - LeetSync

--- 2731-memoize/memoize.js
+++ 2731-memoize/memoize.js
@@ -1,29 +1,19 @@
 /**
  * @param {Function} fn
  */
-// function memoize(fn) {//Define a function called memoize that takes in a function fn as its parameter.
-// let cache={}//Created an empty object called cache to store previously computed results
-//     return function(...args) {//Return a new function that takes in any number of arguments using the spread syntax.
-//         let key=JSON.stringify(args)//convert the arguments into a string to use as a key for the cache object
+function memoize(fn) {//Define a function called memoize that takes in a function fn as its parameter.
+let cache={}//Created an empty object called cache to store previously computed results
+    return function(...args) {//Return a new function that takes in any number of arguments using the spread syntax.
+        let key=JSON.stringify(args)//convert the arguments into a string to use as a key for the cache object
 
-//        if(cache[key]!==undefined) return cache[key] //if the key already exists in the cache object. If it does, return the cached value.
-//        //If the key doesn't exist in the cache, call the original function fn using apply() to pass the arguments and store the result in the cache object using the key as the property name.Finally, return the computed result.
-//        const result=fn.apply(this,args)
-//        cache[key]=result
-//     return result
-//     }
-// }
-function memoize(fn){
-    let cache={}
-    return function(...args){
-        let key =JSON.stringify(args)
-        if(cache[key]!==undefined) return cache[key]
-
-        const result=fn.apply(this,args)
-        cache[key]=result
-return result
+       if(cache[key]!==undefined) return cache[key] //if the key already exists in the cache object,return the cached value.
+       //else call the original function fn using apply() to pass the arguments and store the result in the cache object using the key as the property name.
+       const result=fn.apply(this,args)
+       cache[key]=result
+    return result
     }
 }
+
 /** 
  * let callCount = 0;
  * const memoizedFn = memoize(function (a, b) {
