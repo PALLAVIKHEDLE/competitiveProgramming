f016cee8b664149dd0d15a5ba6ca11ea98d12825
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-09-10 19:38:37 -0700

    Time: 2313 ms (7.35%) | Memory: 66.6 MB (98.12%) - LeetSync

--- 148-sort-list/sort-list.js
+++ 148-sort-list/sort-list.js
@@ -10,47 +10,52 @@
  * @return {ListNode}
  */
 var sortList = function(head) {
-    // if(!head)return null
-    // let dummyNode= new ListNode(0,head)
-
-    // let curr=head.next,prev=head
-    // while(curr){
-    //     if(prev<curr){
-    //         prev=curr
-    //         curr=curr.next
-    //     }else{
-    //         if(prev>dummyNode){
-    //             prev=
-    //         }
-    //     }
-    // }
-
     if(!head)return null
-    let prev=head, curr=head.next
-    let dummyNode= new ListNode(0, head)
-  //ListNode class with a value of 0 and its next pointing to the original head of the linked list.
+    let dummyNode= new ListNode(0,head)
+    let curr=head.next,prev=head
     while(curr){
-        //if it's already sorted, we move on
-        if(curr.val>=prev.val){
+        if(prev.val<=curr.val){
             prev=curr
             curr=curr.next
         }else{
-            //iterate over since the beginning until we find the place to add
             let temp=dummyNode
-            // console.log('temp',temp)
-            // console.log('next',temp.next.val, curr.val)
             while(curr.val>temp.next.val){
                 temp=temp.next
             }
-            // console.log('remove',prev.next, curr.next, temp.next, curr)
-            //remove the current 
             prev.next=curr.next
-            //insert current between
             curr.next=temp.next
             temp.next=curr
-            // things sorted out, let's move to the next pointer
-            curr = prev.next;
+            curr=prev.next
         }
     }
-return dummyNode.next
+    return dummyNode.next
+
+//     if(!head)return null
+//     let prev=head, curr=head.next
+//     let dummyNode= new ListNode(0, head)
+//   //ListNode class with a value of 0 and its next pointing to the original head of the linked list.
+//     while(curr){
+//         //if it's already sorted, we move on
+//         if(curr.val>=prev.val){
+//             prev=curr
+//             curr=curr.next
+//         }else{
+//             //iterate over since the beginning until we find the place to add
+//             let temp=dummyNode
+//             // console.log('temp',temp)
+//             // console.log('next',temp.next.val, curr.val)
+//             while(curr.val>temp.next.val){
+//                 temp=temp.next
+//             }
+//             // console.log('remove',prev.next, curr.next, temp.next, curr)
+//             //remove the current 
+//             prev.next=curr.next
+//             //insert current between
+//             curr.next=temp.next
+//             temp.next=curr
+//             // things sorted out, let's move to the next pointer
+//             curr = prev.next;
+//         }
+//     }
+// return dummyNode.next
 };
