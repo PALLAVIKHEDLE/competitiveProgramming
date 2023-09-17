229fb4d15e13a296630111855bd39756529aee6d
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-09-16 20:07:28 -0700

    Time: 56 ms (79.21%) | Memory: 45.1 MB (24.26%) - LeetSync

---
+++ next/2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var divideArray = function(nums) {
+    let map={}
+    for(let i=0;i<nums.length;i++){
+        let val=map[nums[i]]
+        if(val==undefined)map[nums[i]]=1
+        else map[nums[i]]=val+1
+    }
+      // Check if all elements meet the conditions
+    for (key in map) {
+        if (nums.length % 2 == 0 && map[key] % 2 != 0) return false;
+        else if (nums.length % 2 != 0 && map[key] % 2 != 1) return false;
+    }
+    
+    return true;
+};
