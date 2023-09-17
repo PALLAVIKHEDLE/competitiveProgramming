b70b312c89c20b0e94d919836cfedaef29eaa715
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-09-17 12:31:47 -0700

    Time: 180 ms (26.26%) | Memory: 69.6 MB (8.13%) - LeetSync

--- next/215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
+++ next/215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
@@ -4,6 +4,7 @@
  * @return {number}
  */
 var findKthLargest = function(nums, k) {
+//1. 
     // let map={}, resultArray=[]
     // for(let i=0; i<nums.length;i++){
     //     let val=map[nums[i]]
@@ -17,8 +18,33 @@ var findKthLargest = function(nums, k) {
 
     // let sorted=resultArray.sort((a,b)=>b-a)
     // console.log('sorted',sorted, sorted[k-1])
-
-    nums.sort((a, b) => b - a);
-    return nums[k-1];
+    
+//2. 
+    // nums.sort((a, b) => b - a);
+    // return nums[k-1];
+    
+//3. 
+        let minHeap=new MinPriorityQueue()
+        nums.forEach((num)=>add(num))
+        
+    
+   function add(val){
+        let isUnderCapacity=minHeap.size()<k
+        if(isUnderCapacity){
+            minHeap.enqueue(val)
+            return top()
+        }
+        
+        let isLarger=top()<val
+        if(isLarger){
+            minHeap.dequeue()
+            minHeap.enqueue(val)
+        }
+       return top()  
+    }
+     function  top () {
+        return minHeap.front()?.element || 0
+    }
+return top()
     
 };
