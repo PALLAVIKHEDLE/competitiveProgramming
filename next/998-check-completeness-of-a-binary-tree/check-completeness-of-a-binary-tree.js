41e0bd21786051cb8f83d8836edb98c48dbcd0a1
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-09-27 10:48:50 -0700

    Time: 71 ms (13.77%) | Memory: 44.4 MB (60.87%) - LeetSync

---
+++ next/998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isCompleteTree = function(root) {
+    let seenNull=false
+    if(!root)return null
+    let queue=[root]
+    while(queue.length){
+       const result=[]
+        for(let nextNode of queue){
+            if(!nextNode) seenNull=true
+            else{
+                if(seenNull)return false
+                result.push(nextNode.left,nextNode.right)
+            }
+        }
+        queue=result
+    }
+return true
+};
