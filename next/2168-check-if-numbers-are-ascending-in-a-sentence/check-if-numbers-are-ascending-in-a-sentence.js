5f945fad2be6d20ef8aed4c5c3ac72ea27d02125
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-09-28 13:31:12 -0700

    Time: 57 ms (21.01%) | Memory: 42.3 MB (17.65%) - LeetSync

--- next/2168-check-if-numbers-are-ascending-in-a-sentence/check-if-numbers-are-ascending-in-a-sentence.js
+++ next/2168-check-if-numbers-are-ascending-in-a-sentence/check-if-numbers-are-ascending-in-a-sentence.js
@@ -5,17 +5,12 @@
 var areNumbersAscending = function(s) {
 let arr = s.split(" ");
 const numbers = [];
-
     for (let char of arr) {
         if(parseInt(char)&& parseInt(char)>=0) numbers.push(parseInt(char))
     }
-  
-console.log('numbers',numbers)
-    for (let i = 0; i < numbers.length - 1; i++) {
-        if (Number(numbers[i]) >= Number(numbers[i + 1])) {
-            return false;
-        }
-    }
 
-    return true;
+    for (let i = 0; i < numbers.length - 1; i++) {
+        if (Number(numbers[i]) >= Number(numbers[i + 1]))return false;   
+    }
+return true;
 };
