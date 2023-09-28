79191e2cbaeed1c11f307bcc02502b0dbe8ac410
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-09-28 11:00:48 -0700

    Time: 50 ms (88.57%) | Memory: 42.5 MB (69.05%) - LeetSync

--- next/2341-count-prefixes-of-a-given-string/count-prefixes-of-a-given-string.js
+++ next/2341-count-prefixes-of-a-given-string/count-prefixes-of-a-given-string.js
@@ -4,10 +4,17 @@
  * @return {number}
  */
 var countPrefixes = function(words, s) {
+    //1st Approach
+//     let count=0
+//     for(let word of words){
+//         if(s.startsWith(word))count++
+//     }  
+// return count    
+
+    //2nd Approach
     let count=0
-    for(let word of words){
-        if(s.startsWith(word))count++
+    for(let i=0; i<words.length;i++){
+        if(words[i]===s.slice(0,words[i].length))count++
     }
-    
-return count    
+return count
 };
