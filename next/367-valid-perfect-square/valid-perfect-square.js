7db2e2c05bf8f36aa72f5f8f2943ef79db0a4a84
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-10-04 11:19:17 -0700

    Time: 39 ms (98.45%) | Memory: 42 MB (25.21%) - LeetSync

---
+++ next/367-valid-perfect-square/valid-perfect-square.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function(num) {
+    let start=1,end=num
+    while(start<=end){
+        let mid=Math.floor((start+end)/2)
+        if(mid*mid==num)return true
+        else if(mid*mid>num)end=mid-1
+        else start=mid+1
+    } 
+return false
+};
