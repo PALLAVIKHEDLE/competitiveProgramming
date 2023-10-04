9142208a6c48696f104001ea4ce73d9491cb6f30
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-10-04 11:58:56 -0700

    Time: 61 ms (41.82%) | Memory: 42.4 MB (38.18%) - LeetSync

---
+++ next/633-sum-of-square-numbers/sum-of-square-numbers.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} c
+ * @return {boolean}
+ */
+var judgeSquareSum = function(c) {
+    let end=Math.floor(Math.sqrt(c)), start=0
+    while(start<=end){
+        let sum=start**2+end**2
+        if(sum==c)return true
+        else if(sum<c)start++
+        else end--
+    }
+return false
+};
