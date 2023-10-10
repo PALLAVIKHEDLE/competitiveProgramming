e5f27702774beda95ecb83ca605f7cd3a5599d87
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-10-10 10:47:16 -0700

    Time: 65 ms (5.47%) | Memory: 42.4 MB (26.17%) - LeetSync

---
+++ next/92-reverse-linked-list-ii/reverse-linked-list-ii.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} left
+ * @param {number} right
+ * @return {ListNode}
+ */
+var reverseBetween = function(head, left, right) {
+   //Two pointer
+   if(!head ||left==right) return head
+  //Create a dummy node and connect its next to the head of the list.
+   let dummy=new ListNode(0)
+   dummy.next=head
+   let prev=dummy//Initialize a prev pointer to the dummy node
+   for(let i=0; i<left-1;i++){
+       prev=prev.next
+   }
+   let curr=prev.next
+   for(let i=0; i<right-left; i++){
+       const nextNode=curr.next
+       curr.next=nextNode.next
+       nextNode.next=prev.next
+       prev.next=nextNode
+   }
+return dummy.next
+};
