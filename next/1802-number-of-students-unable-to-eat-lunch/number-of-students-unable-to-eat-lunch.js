97b41d415a0b260f5b8da640e8e63c7f37a963db
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-10-17 10:36:48 -0700

    Time: 53 ms (50.82%) | Memory: 41.7 MB (85.71%) - LeetSync

---
+++ next/1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} students
+ * @param {number[]} sandwiches
+ * @return {number}
+ */
+var countStudents = function(students, sandwiches) {
+    let count=0
+    // if count=students.length, means there's an infinite-loop
+    while(students.length && count<students.length){
+        if(students[0]==sandwiches[0]){
+            count=0
+            // CASE1: student gets a sandwich and leaves.
+            students.shift();
+            sandwiches.shift()  
+        }else{
+            count++
+            // CASE2: student goes to queue's end.
+            students.push(students.shift())
+        }
+    }
+return students.length    
+};
