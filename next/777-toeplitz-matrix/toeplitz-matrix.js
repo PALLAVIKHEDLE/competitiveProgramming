f30a49d035e63ff2c8b8e1a340793f2cb4c09cb9
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-10-30 19:18:25 -0700

    Time: 54 ms (74.78%) | Memory: 43.9 MB (58.46%) - LeetSync

--- next/777-toeplitz-matrix/toeplitz-matrix.js
+++ next/777-toeplitz-matrix/toeplitz-matrix.js
@@ -2,10 +2,10 @@
  * @param {number[][]} matrix
  * @return {boolean}
  */
-var isToeplitzMatrix = function(matrix) {
-    
+var isToeplitzMatrix = function(matrix) { 
     for(let i=1; i<matrix.length; i++){
         for(let j=1; j<matrix[i].length;j++){
+            //current element (matrix[i][j]) is not equal to the one diagonally above and to the left (matrix[i-1][j-1]).
             if(matrix[i][j]!==matrix[i-1][j-1]) return false
         }
     }
