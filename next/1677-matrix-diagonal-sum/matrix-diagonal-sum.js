37152f6103358d6726008ec32082f7186b2a50ac
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-10-31 14:30:31 -0700

    Time: 54 ms (53.76%) | Memory: 43.3 MB (84.83%) - LeetSync

--- next/1677-matrix-diagonal-sum/matrix-diagonal-sum.js
+++ next/1677-matrix-diagonal-sum/matrix-diagonal-sum.js
@@ -3,15 +3,19 @@
  * @return {number}
  */
 var diagonalSum = function(mat) {
-    let row=mat.length, sum=0
-   
-    for (let i = 0; i < row; i++) {
-        for (let j = 0; j < mat[i].length; j++) {
-            // Add the diagonal elements
-            if (i === j || i + j === row - 1) {
-                sum = sum + mat[i][j];
-            }
+    let row=mat.length-1, sum=0
+    //O(n^2)
+//     for (let i = 0; i < row; i++) {
+//         for (let j = 0; j < mat[i].length; j++) {
+//             // Add the diagonal elements
+//             if (i === j || i + j === row - 1) sum = sum + mat[i][j];     
+//         }
+//     }
+//  return sum   
+        for (let i = 0; i <=row; i++) {
+            //Main diagonal
+            sum = sum + mat[i][i];
+            if (i !== (row - i)) sum = sum + mat[i][row-i];     
         }
-    }
- return sum   
+        return sum   
 };
