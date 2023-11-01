eeda2a7a6a3d0fa6c63658de05effe11dd95170a
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-11-01 14:11:30 -0700

    Time: 341 ms (6.60%) | Memory: 49.8 MB (5.66%) - LeetSync

--- next/1386-shift-2d-grid/shift-2d-grid.js
+++ next/1386-shift-2d-grid/shift-2d-grid.js
@@ -1,11 +1,11 @@
 var shiftGrid = function(grid, k) {
-    let rows = grid.length,
-        cols = grid[0].length;
+    let rows = grid.length
 //perform the shifting k times
      for(let i=0; i<k; i++){
          //iterate over row
          for(let j=0; j<rows; j++){
 //extract last element of the c/n row & move it to the beggining of next row
+console.log('rows',rows)
         grid[j].unshift(grid[((rows+j-1)%rows)].pop());
          }
      }   
