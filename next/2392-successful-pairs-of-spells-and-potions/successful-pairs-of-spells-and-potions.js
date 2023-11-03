acda4af8c1addc940b1587bc959e4f0ce4fcda14
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-11-03 11:09:54 -0700

    Time: 240 ms (59.20%) | Memory: 72.3 MB (44.59%) - LeetSync

---
+++ next/2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} spells
+ * @param {number[]} potions
+ * @param {number} success
+ * @return {number[]}
+ */
+var successfulPairs = function(spells, potions, success) {
+    potions.sort((a,b)=>a-b)
+     let res=[];
+
+    for(s of spells){
+     let left=0, right=potions.length-1;
+
+        while(left<=right){
+            let mid=Math.floor((left+right)/2)
+            if(s*potions[mid]>=success){
+                right=mid-1;
+                index=mid;        
+            }
+            else left=mid+1
+        }
+        res.push(potions.length-left)
+    }   
+return res
+};
