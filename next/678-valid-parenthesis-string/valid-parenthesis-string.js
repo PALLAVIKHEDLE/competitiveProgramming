2f1bc5580facf4da6835b6e529f4312e69a67ffb
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-11-03 13:18:25 -0700

    Time: 46 ms (81.50%) | Memory: 41.4 MB (89.43%) - LeetSync

--- next/678-valid-parenthesis-string/valid-parenthesis-string.js
+++ next/678-valid-parenthesis-string/valid-parenthesis-string.js
@@ -3,25 +3,25 @@
  * @return {boolean}
  */
 var checkValidString = function(s) {
-    let validChar=[], starChar=[]
-    
+    let validChar=[], starCheck=[]
     for(let i=0; i<s.length;i++){
-        if(s[i]=='(') validChar.push(i)
-        else if(s[i]=='*')starChar.push(i)
-        else {
-            if(validChar.length>0){
-                validChar.pop()
-            }else if(starChar.length>0)  starChar.pop()
+        //Identify the star & valid parenthesis position
+        if(s[i]=='(')validChar.push(i)
+        else if(s[i]=='*')starCheck.push(i)
+        else{
+            if(validChar.length>0) validChar.pop()//Pop from 'validChar' if '(' is available
+            else if(starCheck.length>0) starCheck.pop()//Pop from 'starCheck' if '*' is available
             else return false
         }
     }
-    let i=validChar.length-1, j=starChar.length-1
-    while(validChar[i]<starChar[j]){
-           validChar.pop()
-            starChar.pop()
-            i--
-            j--
+// Matching Parentheses with Wildcards
+    let i = validChar.length - 1, j = starCheck.length - 1;
+    while(validChar[i]<starCheck[j]){
+        validChar.pop();
+        starCheck.pop();
+        i--;
+        j--;
     }
-    if(validChar.length==0) return true
-    else return false
+     if(validChar.length === 0) return true;
+     else return false;
 };     
