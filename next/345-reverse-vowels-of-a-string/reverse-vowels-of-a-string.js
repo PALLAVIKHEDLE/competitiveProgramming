e3d19b1c4b1d15fa7a2bfeebe19bb38b206fa5fb
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-11-04 10:22:43 -0700

    Time: 69 ms (57.78%) | Memory: 51.2 MB (26.60%) - LeetSync

--- next/345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.js
+++ next/345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.js
@@ -2,19 +2,39 @@
  * @param {string} s
  * @return {string}
  */
+// var reverseVowels = function(s) {
+//    let start=0, end=s.length, str=s.split('')
+//    let vowels=['a','e','i','o','u','A','E','I','O','U']
+//     while(start<end){
+//         if(!vowels.includes(str[start]))start++  
+//         else if(!vowels.includes(str[end]))end-- 
+//         else{
+//             let temp=str[start]
+//             str[start]=str[end]
+//             str[end]=temp
+//             start++
+//             end--   
+//         }  
+//     }
+// return str.join('')
+// };
 var reverseVowels = function(s) {
-   let start=0, end=s.length, str=s.split('')
-   let vowels=['a','e','i','o','u','A','E','I','O','U']
-    while(start<end){
-        if(!vowels.includes(str[start]))start++  
-        else if(!vowels.includes(str[end]))end-- 
-        else{
-            let temp=str[start]
-            str[start]=str[end]
-            str[end]=temp
-            start++
-            end--   
-        }  
+    let start = 0,end = s.length-1
+    let output = s.split('')
+    let vow = ['a', 'e', 'i', 'o', 'u'];
+
+    while (start < end){
+        if (vow.includes(s[start].toLowerCase()) && vow.includes(s[end].toLowerCase())){
+            let temp = output[start];
+            output[start] = output[end];
+            output[end] = temp
+            start++;
+            end--;
+        } if (!vow.includes(s[start].toLowerCase())){
+            start++;
+        } if (!vow.includes(s[end].toLowerCase())){
+            end--;
+        }
     }
-return str.join('')
+    return output.join('');
 };
