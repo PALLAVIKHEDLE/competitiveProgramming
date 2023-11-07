4e5070c02d1bc6b6f9906fce9b0183cab50d8d5d
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-11-06 21:55:14 -0800

    Time: 68 ms (40.00%) | Memory: 46.2 MB (20.00%) - LeetSync

--- next/2039-sum-game/sum-game.js
+++ next/2039-sum-game/sum-game.js
@@ -8,18 +8,13 @@ var sumGame = function(num) {
         let digit=num[i]
         if(digit=='?'){
             // If in the first half or last digit in odd-length string
-            if (i < mid || (i === mid && num.length % 2 === 1)) {
-                sum1 += 4.5; // Adjust for half of 9
-            } else {
-                sum2 += 4.5; // Adjust for half of 9
-            }
+            if (i < mid || (i === mid && num.length % 2 === 1)) sum1 += 4.5; // Adjust for half of 9
+            else sum2 += 4.5; // Adjust for half of 9
         }else{
             let val=parseInt(digit)
             if(i<mid)sum1+=val
             else sum2+=val
         }
- 
-    }
-    
+    }   
    return sum1 !== sum2;
 };
