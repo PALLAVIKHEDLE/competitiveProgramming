f06c327696cd9aeadacd2be983fc1906ff547e49
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-11-08 11:05:08 -0800

    Time: 67 ms (52.17%) | Memory: 48.4 MB (97.32%) - LeetSync

--- next/1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.js
+++ next/1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.js
@@ -5,11 +5,12 @@
  * @return {number}
  */
 var numOfSubarrays = function(arr, k, threshold) {
-   let start=0, count=0, average=0;
+  
+ let start=0, count=0, average=0;
    for(let i=0; i<arr.length;i++) {
        average+=arr[i] 
        while(i-start+1>k) average-=arr[start++]
        if(i-start+1===k&& average/k>=threshold)count++ 
    }  
-return count  
+return count 
 };
