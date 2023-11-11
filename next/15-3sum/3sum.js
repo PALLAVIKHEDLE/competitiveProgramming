d9c22a7002c3b87df1bce084bc438b547af173a7
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-11-10 23:15:25 -0800

    Time: 237 ms (21.46%) | Memory: 62 MB (22.15%) - LeetSync

--- next/15-3sum/3sum.js
+++ next/15-3sum/3sum.js
@@ -2,60 +2,35 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-// var threeSum = function(nums) {
-//   let start=0, end=0, sum=0, map={}, temp=[], conStr=''
-//   while(start<nums.length){
-//       console.log('index', start, end, sum)
-//        sum+=nums[start]
-//        temp.push(nums[start])
-//     while(temp.length==3){
-         
-//         if(sum==0 && temp.length==3){
-//           temp.sort((a,b)=>a-b)
-//           conStr=temp.join('')
-//            if(map[conStr]==undefined)map[conStr]=1
-//            else map[conStr]=map[conStr]+1    
-//         }
-//          sum-=nums[end]  
-//           temp.shift()
-//         end++
-//     }
-//          start++
-//   } 
-//         console.log('map', conStr, map, temp)
-
-// };
-
 var threeSum = function(nums) {
-  nums.sort((a, b) => a - b);
-  const result = [];
+   let result=[];
 
-  for (let i = 0; i < nums.length - 2; i++) {
-    if (i > 0 && nums[i] === nums[i - 1]) continue;
-    
+   nums.sort((a,b)=>a-b)
+   for(let i=0; i<nums.length;i++){
+       let left=i+1, right=nums.length-1; 
 
-    let left = i + 1, right = nums.length - 1;
+       console.log('left',left)
+       if(i>0&& nums[i]==nums[i-1])continue
 
-    while (left < right) {
-      const sum = nums[i] + nums[left] + nums[right];
+       while(left<right){
+           let currentSum=nums[i]+nums[left]+nums[right]
+           if(currentSum==0){
+               result.push([nums[i], nums[left], nums[right]])
+                 // Skip duplicates for the second element
+                while (left < right && nums[left] === nums[left + 1]) {
+                    left++;
+                }
 
-      if (sum === 0) {
-        result.push([nums[i], nums[left], nums[right]]);
-        
-        // Skip duplicate values for the second element of the triplet
-        while (left < right && nums[left] === nums[left + 1])left++;
-        
-
-        // Skip duplicate values for the third element of the triplet
-        while (left < right && nums[right] === nums[right - 1]) right--;
-        
-
-        left++;
-        right--;
-      } else if (sum < 0) left++;
-       else right--;    
-    }
-  }
-
-  return result;
+                // Skip duplicates for the third element
+                while (left < right && nums[right] === nums[right - 1]) {
+                    right--;
+                }
+               left++
+               right--
+           }
+           else if(currentSum<0)left++
+           else right--
+       }
+   }
+ return result  
 };
