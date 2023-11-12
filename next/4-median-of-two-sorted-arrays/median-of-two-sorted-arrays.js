5daf483abb24791a735745b0cd3fe7950019c0e6
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-11-12 10:48:53 -0800

    Time: 413 ms (5.06%) | Memory: 50.6 MB (13.25%) - LeetSync

--- next/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
+++ next/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
@@ -8,7 +8,7 @@ var findMedianSortedArrays = function(nums1, nums2) {
        nums1.push(nums2[i])
        nums1.sort((a,b)=>a-b)
    }
-    let left=0, right=nums1.length-1, {length}=nums1 ,mid=Math.floor((left+right)/2)
-       if(length%2!==0) return nums1[mid]
+    let left=0, right=nums1.length-1,mid=Math.floor((left+right)/2)
+       if(nums1.length%2!==0) return nums1[mid]
        else return (nums1[mid]+nums1[mid+1])/2
 };
