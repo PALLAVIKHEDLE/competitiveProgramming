619446f9ffc1d0a9a1169b1bd0d43cddf174651b
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-11-13 14:57:22 -0800

    Time: 55 ms (57.19%) | Memory: 45 MB (10.86%) - LeetSync

---
+++ next/682-baseball-game/baseball-game.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+var calPoints = function(operations) {
+  let stack=[], sum=0
+  for(let i=0; i<operations.length; i++){
+      if(operations[i]=='D'){
+         let last= 2*stack[stack.length-1]
+         stack.push(last)
+      }
+      else if(operations[i]=='+'){
+         let first= stack[stack.length-1]
+         let second=stack[stack.length-2]
+         stack.push(first+second)    
+      }
+      else if(operations[i]=='C')stack.pop()
+      else stack.push(Number(operations[i]))
+  }
+  console.log('stack', stack)
+
+  for(let i=0; i<stack.length;i++)sum+=stack[i]
+
+return sum  
+};
