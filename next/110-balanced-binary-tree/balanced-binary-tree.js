f83b6d360f657d47e1a60aff2fc94448c7ecf9f2
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-11-14 13:49:15 -0800

    Time: 63 ms (64.72%) | Memory: 47 MB (74.35%) - LeetSync

---
+++ next/110-balanced-binary-tree/balanced-binary-tree.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+   if(!root) return true
+       let heightL=isBalanced(root.left)
+        let heightR=isBalanced(root.right)
+        if(heightL==false||heightR==false) return false//left subtree or right subtree unbalanced
+        if(Math.abs(heightL-heightR)>1)return false
+        return Math.max(heightL, heightR)+1//we add 1(for the root
+};
