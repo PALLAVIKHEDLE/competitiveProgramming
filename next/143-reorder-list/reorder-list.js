ee749603e9d870437dff0369c14a8b5468d31c9a
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-11-22 18:44:58 -0800

    Time: 617 ms (5.03%) | Memory: 49.7 MB (88.13%) - LeetSync

---
+++ next/143-reorder-list/reorder-list.js
@@ -0,0 +1,44 @@
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {void} Do not return anything, modify head in-place instead.
+ */
+
+var reorderList = function(head) {
+  let current=head, tempHead=head,targetNode
+  while(current){
+      let temp=current.next
+      targetNode=getSecondLastNode(current)
+      if (current == targetNode|| targetNode==null) {
+                break
+            }
+      if (targetNode) {
+            current.next=targetNode.next
+            targetNode.next=null
+                if (current.next !== null) {
+                    current.next.next=temp
+                    current=current.next.next
+                }
+      }
+  }
+
+ return tempHead
+};
+
+
+const getSecondLastNode = (head) => {
+if(!head) return head
+let prev = null
+  while(head.next){
+      prev = head
+      head = head.next
+  }
+ return prev
+};
