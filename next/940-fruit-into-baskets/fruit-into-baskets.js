011cdb9cf86fdb9cb5a92e9b15a81af2ad2bd48e
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-11-26 15:17:56 -0800

    Time: 378 ms (14.18%) | Memory: 65.5 MB (18.58%) - LeetSync

--- next/940-fruit-into-baskets/fruit-into-baskets.js
+++ next/940-fruit-into-baskets/fruit-into-baskets.js
@@ -3,23 +3,17 @@
  * @return {number}
  */
 var totalFruit = function(fruits) {
- let map={}, sum=maxSum=start=0
+ let map={}, maxlen=start=0
  for(let i=0; i<fruits.length; i++){
      if(map[fruits[i]]==undefined)map[fruits[i]]=1
      else map[fruits[i]]=map[fruits[i]]+1
  
  while (Object.keys(map).length > 2) {
-      map[fruits[start]] -= 1;
-      if (map[fruits[start]] === 0) {
-        delete map[fruits[start]];
-      }
-      start += 1;
+      map[fruits[start]]--;
+      if (map[fruits[start]] === 0)delete map[fruits[start]];
+      start++
     }
-
-    sum = i - start + 1;
-    maxSum = Math.max(maxSum, sum);
-  
+maxlen = Math.max(maxlen, i - start + 1);
 }
-  return maxSum;
-
+  return maxlen;
 };
