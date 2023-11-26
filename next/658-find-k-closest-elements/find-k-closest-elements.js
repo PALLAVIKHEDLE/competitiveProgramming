8e4345212d9c673a46c72a6982fd317c3d8019b5
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-11-26 00:36:39 -0800

    Time: 81 ms (51.59%) | Memory: 47.8 MB (69.44%) - LeetSync

--- next/658-find-k-closest-elements/find-k-closest-elements.js
+++ next/658-find-k-closest-elements/find-k-closest-elements.js
@@ -5,18 +5,25 @@
  * @return {number[]}
  */
 var findClosestElements = function(arr, k, x) {
- let result=[], diffsWithIndices=[];
-    for(let i=0; i<arr.length; i++){
-        let diff=Math.abs(x-arr[i])  
-        diffsWithIndices.push({diff,index:i})
-    }
-    diffsWithIndices.sort((a,b)=>a.diff-b.diff)// Sort by absolute differences
+//  let result=[], diffsWithIndices=[];
+//     for(let i=0; i<arr.length; i++){
+//         let diff=Math.abs(x-arr[i])  
+//         diffsWithIndices.push({diff,index:i})
+//     }
+//     diffsWithIndices.sort((a,b)=>a.diff-b.diff)// Sort by absolute differences
 
-      // Take the first k elements with the smallest absolute differences
-    for(let j=0; j<k; j++){
-        result.push(arr[diffsWithIndices[j].index])
+//       // Take the first k elements with the smallest absolute differences
+//     for(let j=0; j<k; j++){
+//         result.push(arr[diffsWithIndices[j].index])
+//     }
+//     // Sort the result array to maintain the order
+//   result.sort((a, b) => a - b);
+//   return result;
+
+  let end=arr.length-1,start=0
+    while(end-start>=k){
+        if(Math.abs(arr[start]-x)<=Math.abs(arr[end]-x)) end--
+        else start++
     }
-      // Sort the result array to maintain the order
-  result.sort((a, b) => a - b);
-  return result;
+    return arr.slice(start, end+1)
 }
