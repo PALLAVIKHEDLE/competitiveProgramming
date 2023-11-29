df4dc44543d92bb3c16325048f35d8c8eacac302
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-11-29 10:38:27 -0800

    Time: 91 ms (38.24%) | Memory: 48.8 MB (30.51%) - LeetSync

--- next/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ next/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -5,15 +5,17 @@
 
 
 var lengthOfLongestSubstring = function(s) {
-    let map = {}, start = end = 0, max = 0;
+    let map = {}, start=end =max=0;
 
     while (end < s.length) {
+        // If the character at the end index is not present in the map,
+        //If the character at the end index is already present in the map and has a count of 0
         if (map[s[end]] === undefined || map[s[end]] === 0) {
             map[s[end]] = 1;
             end++;
             max = Math.max(max, end - start);
         } else {
-            map[s[start]] = 0; // Reset the count for the character at start index
+            map[s[start]] = 0; 
             start++;
         }
     }
