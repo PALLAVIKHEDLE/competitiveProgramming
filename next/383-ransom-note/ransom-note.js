ea2b137b5af40a01cd3a63ec5e563922dc5cab98
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-11-29 18:36:54 -0800

    Time: 72 ms (71.96%) | Memory: 43.1 MB (97.34%) - LeetSync

---
+++ next/383-ransom-note/ransom-note.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+     let map = {};
+    
+    for (let i = 0; i < ransomNote.length; i++) {
+        map[ransomNote[i]] == undefined ? map[ransomNote[i]] = 1 : map[ransomNote[i]]++;
+    }
+
+    for (let i = 0; i < magazine.length; i++) {
+        if (map[magazine[i]] && map[magazine[i]] > 0) {
+            map[magazine[i]]--;
+        }
+    }
+
+    for (let key in map) {
+        if (map[key] > 0) {
+            return false;
+        }
+    }
+
+    return true; 
+};
