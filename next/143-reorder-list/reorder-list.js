3aa420bfaad12ea169c36d049151a0954997fe27
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-11-30 10:31:21 -0800

    Time: 613 ms (5.01%) | Memory: 50 MB (68.78%) - LeetSync

--- next/143-reorder-list/reorder-list.js
+++ next/143-reorder-list/reorder-list.js
@@ -10,43 +10,28 @@
  * @return {void} Do not return anything, modify head in-place instead.
  */
 var reorderList = function(head) {
-    let current=head, tempHead=head
-    while(current){
-    let nextTemp=current.next
-    let newOrder=getSecondLastElement(current)
-    if (current == newOrder|| newOrder==null) break
-    console.log('newOrder', newOrder)
- 
-    current.next=newOrder.next
-    newOrder.next=null
-    current.next.next=nextTemp
-    current=current.next.next
-    }
- return tempHead   
+   let current=head, tempHead=head
+  while(current){
+      let temp=current.next
+      let targetNode=getSecondLastNode(current)
+      if (current == targetNode|| targetNode==null) break
+      current.next=targetNode.next
+      targetNode.next=null
+      if (current.next !== null) {
+          current.next.next=temp
+          current=current.next.next
+      }
+  }
+ return tempHead
 };
-function getSecondLastElement(current){
-    if(!current) return current
-    let prev=null
-    while(current.next){
-        prev=current
-        current=current.next
-    }
-return prev    
+
+
+const getSecondLastNode = (head) => {
+if(!head) return head
+let prev = null
+  while(head.next){
+      prev = head
+      head = head.next
+  }
+ return prev
 }
-
-
-
-//  let current=head, tempHead=head
-//   while(current){
-//       let temp=current.next
-//       let targetNode=getSecondLastNode(current)
-//       if (current == targetNode|| targetNode==null) break
-//       current.next=targetNode.next
-//       targetNode.next=null
-//       if (current.next !== null) {
-//           current.next.next=temp
-//           current=current.next.next
-//       }
-//   }
-//  return tempHead
-// };
