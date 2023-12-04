c0e8d1719c65df877373b9168946c4101215a993
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-12-03 22:35:49 -0800

    Time: 62 ms (55.66%) | Memory: 43.6 MB (98.42%) - LeetSync

---
+++ next/150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+    let stack=[], num1, num2;
+    
+    const operators={
+        '+':(num1,num2)=>num1+num2,
+        '-':(num1,num2)=>num1-num2,
+        '*':(num1,num2)=>num1*num2,
+        '/':(num1,num2)=>Math.trunc(num1/num2)
+    }
+
+    for(let i=0; i<tokens.length; i++){
+        if(operators[tokens[i]]){
+            num2=stack.pop()
+            num1=stack.pop()
+            stack.push(operators[tokens[i]](num1,num2))
+        }else stack.push(parseInt(tokens[i]))
+    }
+return stack.pop()    
+};
