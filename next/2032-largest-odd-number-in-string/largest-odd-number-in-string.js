be0d3aa9f0fcd648a4c64598a922777a9974456c
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-12-07 08:45:51 -0800

    Time: 63 ms (50.49%) | Memory: 46.9 MB (15.66%) - LeetSync

--- next/2032-largest-odd-number-in-string/largest-odd-number-in-string.js
+++ next/2032-largest-odd-number-in-string/largest-odd-number-in-string.js
@@ -2,11 +2,12 @@
  * @param {string} num
  * @return {string}
  */
-var largestOddNumber = function(num) {
-       let max ="";
-    for (let i =num.length-1; i>=0; i--) {
-       let val=parseInt(num[i])
-        if(val%2!==0) return num.substring(0,i+1)
+
+ var largestOddNumber = function(num) {
+    num = num.toString(); // Convert number to string
+    for (let i = num.length - 1; i >= 0; i--) {
+        if (num[i] % 2 !== 0) return num.slice(0, i + 1); //string up to the current index
+        else num = num.slice(0, -1); // Remove the last character of the string
     }
- return max
+return '';
 };
