50cbcfe0aefbf323dafebed8dbec67cb548c3e64
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-12-06 16:17:27 -0800

    Time: 209 ms (5.09%) | Memory: 51.3 MB (5.09%) - LeetSync

--- next/1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.js
+++ next/1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.js
@@ -3,14 +3,21 @@
  * @return {number}
  */
 var totalMoney = function(n) {
- let ans = 0,week = 1,additional = 0;
+//  let ans = 0,week = 1,additional = 0;
 
-    for (let i = 1; i <= n; i++) {
-        ans += week + additional;
-        if (i % 7 === 0) {
-            week++;
-            additional = 0;
-        } else additional++;
+//     for (let i = 1; i <= n; i++) {
+//         ans += week + additional;
+//         if (i % 7 === 0) {
+//             week++;
+//             additional = 0;
+//         } else additional++;
+//     }
+// return ans; 
+ let ans = 0
+
+    for (let i = 0; i < n; i++) {
+       console.log('i%7',i%7,i/7)
+       ans+=Math.floor(i/7)+1+(i%7)
     }
 return ans; 
 };
