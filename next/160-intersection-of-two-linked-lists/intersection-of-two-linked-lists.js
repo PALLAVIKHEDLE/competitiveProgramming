fd35a7b7dfd111b7d6cadaf14f1486328f271842
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-12-08 10:43:46 -0800

    Time: 77 ms (59.12%) | Memory: 49.7 MB (76.54%) - LeetSync

---
+++ next/160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+    let curA = headA, curB = headB
+   if(!headA || !headB)return null
+    while (curA !== curB) {
+        curA = curA == null ? headB : curA.next;
+        curB = curB == null ? headA : curB.next;   
+    }
+return curA;
+};
