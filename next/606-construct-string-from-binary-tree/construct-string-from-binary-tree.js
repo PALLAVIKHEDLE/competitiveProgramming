25ba6050c475461a2b50c6311ba05f436815b649
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-12-08 10:31:06 -0800

    Time: 68 ms (30.92%) | Memory: 46.4 MB (89.47%) - LeetSync

--- next/606-construct-string-from-binary-tree/construct-string-from-binary-tree.js
+++ next/606-construct-string-from-binary-tree/construct-string-from-binary-tree.js
@@ -11,12 +11,20 @@
  * @return {string}
  */
 var tree2str = function(root) {
+    //FIRST(PRE_ORDER)
      if (!root) return '';
     let left = tree2str(root.left);
     let right = tree2str(root.right);
     if (!left && !right) return root.val.toString();
     if (!right) return `${root.val}(${left})`;
-    
     return `${root.val}(${left})(${right})`;
 
+
+    //SECOND APPROACH
+    // if(!root) return ''
+    // let left=tree2str(root.left)
+    // let right=tree2str(root.right)
+    // if(right) return `${root.val}(${left})(${right})`
+    // else if(left) return `${root.val}(${left})`
+    // else return `${root.val}`   
 };
