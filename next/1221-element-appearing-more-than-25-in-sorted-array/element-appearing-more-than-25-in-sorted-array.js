9e2c1818cccf41ae4bbc6ab76a8414b451fd22f0
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-12-10 18:29:59 -0800

    Time: 66 ms (31.38%) | Memory: 49.6 MB (11.17%) - LeetSync

--- next/1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.js
+++ next/1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.js
@@ -3,14 +3,12 @@
  * @return {number}
  */
 var findSpecialInteger = function(arr) {
-    let {length}=arr, map={},max=0
+    let  map={},max=0
     for(count of arr){
         if(map[count]==undefined)map[count]=1
         else map[count]=map[count]+1
         max=Math.max(max,map[count])
     }
-    for(key in map){
-     if(map[key]==max)return key
-    }
 
+    for(key in map)if(map[key]==max)return key
 };
