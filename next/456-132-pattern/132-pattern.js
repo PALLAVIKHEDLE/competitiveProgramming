169bc181b6a747787956cd5eaf36f06ce3dc8e84
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-12-12 08:57:30 -0800

    Time: 73 ms (74.09%) | Memory: 57.9 MB (74.61%) - LeetSync

---
+++ next/456-132-pattern/132-pattern.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var find132pattern = function(nums) {
+    let min=Number.MIN_SAFE_INTEGER, stack=[]
+    for(let i=nums.length-1;i>=0;i--){
+        while(nums[i]>stack[stack.length-1]){
+            min=stack.pop()
+        }
+        if(nums[i]<min)return true
+        stack.push(nums[i])
+    }
+    return false
+};
