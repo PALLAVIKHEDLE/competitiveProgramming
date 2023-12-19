f3ce9bd7ff8c0da95010b44d31158d30d4cafd5e
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-12-19 13:51:31 -0800

    Time: 45 ms (96.94%) | Memory: 43.6 MB (90.16%) - LeetSync

---
+++ next/179-largest-number/largest-number.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var largestNumber = function(nums) {
+     nums.sort((a,b)=>`${b}${a}`-`${a}${b}`)//It then subtracts the second string (${a}${b}) from the first string (${b}${a}). JavaScript automatically converts these strings to numbers before performing the subtraction.
+    if(nums[0]===0) return '0'
+
+ return nums.join('')   
+};
