f7dc6537c28aa8b0519d55af6097372430fc8ad3
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-12-20 17:15:36 -0800

    Time: 154 ms (10.80%) | Memory: 52.8 MB (6.26%) - LeetSync

--- next/209-minimum-size-subarray-sum/minimum-size-subarray-sum.js
+++ next/209-minimum-size-subarray-sum/minimum-size-subarray-sum.js
@@ -3,21 +3,19 @@
  * @param {number[]} nums
  * @return {number}
  */
-
 var minSubArrayLen = function(target, nums) {
-  let sum=0, end=start=0,min=Number.MAX_SAFE_INTEGER;
+    let start=0, sum=0, min=Number.MAX_SAFE_INTEGER, windowLength=0
+    for(let i=0;i<nums.length;i++){
+        sum+=nums[i]
+        while(sum>=target){
+            windowLength=i-start+1
+            console.log('length',windowLength)
+            min=Math.min(windowLength,min)
+            sum-=nums[start]
+            start++
+        }
+    }
+    if(windowLength==0)return 0
 
-  while(start<nums.length){
-      console.log('start', start)
-      sum+=nums[start]
-      while(sum>=target){
-        min=Math.min(min, start-end+1)
-        sum=sum-nums[end]
-        end++
-      }
-        start++
-  }
-if(end==0)return 0
-
-return min
+return min   
 };
