3979f8aceac6f1b2c0d57298e25b36798e2bab93
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-12-22 12:31:33 -0800

    Time: 47 ms (82.36%) | Memory: 41.5 MB (88.33%) - LeetSync

--- next/2813-to-be-or-not-to-be/to-be-or-not-to-be.js
+++ next/2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -3,16 +3,16 @@
  * @return {Object}
  */
 var expect = function(val) {
-    return {
-        toBe: (val2) => {
-            if (val !== val2) throw new Error("Not Equal");
-            else return true;
-        },
-        notToBe: (val2) => {
-            if (val === val2) throw new Error("Equal");
-            else return true;
-        }
-    }
+   return {
+       toBe:(val1)=>{
+           if(val!==val1) throw new Error("Not Equal")
+           else return true
+       },
+       notToBe:(val1)=>{
+           if(val===val1)throw new Error('Equal')
+           else return true
+       }
+   }
 };
 
 /**
