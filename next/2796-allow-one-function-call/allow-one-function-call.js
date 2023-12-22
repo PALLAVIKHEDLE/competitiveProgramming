608fd308a1f16dcda7248ff4aa926190b266ccd5
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-12-22 13:44:13 -0800

    Time: 57 ms (22.23%) | Memory: 41.6 MB (83.65%) - LeetSync

---
+++ next/2796-allow-one-function-call/allow-one-function-call.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let result, usedOnce=false
+    return function(...args){
+       if(!usedOnce){
+           result=fn(...args)
+           usedOnce=true
+return result
+
+       }
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
