c315c8a18521a140afea84361056a7e88c0471d2
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-12-22 13:48:19 -0800

    Time: 279 ms (63.86%) | Memory: 107.9 MB (46.50%) - LeetSync

---
+++ next/2731-memoize/memoize.js
@@ -0,0 +1,26 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    let cache={}
+    return function(...args) {
+        let key=JSON.stringify(args)
+        if(cache[key]!==undefined)return cache[key]
+        const result=fn.apply(this,args)
+        cache[key]=result
+        return result
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
