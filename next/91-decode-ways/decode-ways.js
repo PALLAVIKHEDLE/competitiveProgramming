8daf33b69160f77b43f96f063e4d5ae928ec73a8
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-12-25 01:15:10 -0800

    Time: 62 ms (32.49%) | Memory: 42.4 MB (69.00%) - LeetSync

--- next/91-decode-ways/decode-ways.js
+++ next/91-decode-ways/decode-ways.js
@@ -6,18 +6,14 @@ var numDecodings = function(s) {
  let memo = {};
 
     function recursiveDecode(index) {
+// Base case: If index reaches the end of the string
         if (index === s.length) return 1;
-        
         if (s[index] === '0') return 0;
-        
+// If result for the current index is already calculated, return the memoized value.
         if (memo[index] !== undefined)return memo[index];
         
-
         let ways = recursiveDecode(index + 1);
-
-        if (index + 1 < s.length && parseInt(s.substring(index, index + 2)) <= 26) {
-            ways += recursiveDecode(index + 2);
-        }
+        if (index + 1 < s.length && parseInt(s.substring(index, index + 2)) <= 26)ways += recursiveDecode(index + 2);
 
         memo[index] = ways;
         return ways;
