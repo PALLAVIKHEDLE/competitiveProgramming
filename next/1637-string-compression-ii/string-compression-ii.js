ab0e81148bbd254f600bd44cc114c7e309e45463
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-12-28 01:59:08 -0800

    Time: 1537 ms (33.33%) | Memory: 130 MB (33.33%) - LeetSync

--- next/1637-string-compression-ii/string-compression-ii.js
+++ next/1637-string-compression-ii/string-compression-ii.js
@@ -7,12 +7,13 @@ var getLengthOfOptimalCompression = function(s, k) {
   const memo = new Map();
   return backtrack(0, '', 0, k, memo, s);
 };
-  
+   //i (current index in the string s), prev (previous character), count (count of consecutive characters), k (remaining deletions allowed), memo (memoization map), and s (the input string).
 function backtrack(i, prev, count, k, memo, s) {
   if (k < 0) return Infinity;
   if (i === s.length) return 0;
-  
+  ///unique key for the current set of parameters using a string template.
   const memoKey = `${i}_${prev}_${count}_${k}`;
+  //it checks if the result for these parameters is already memoized. If it is, the function returns the memoized result.
   if (memo.has(memoKey)) return memo.get(memoKey);
   
   let val;
