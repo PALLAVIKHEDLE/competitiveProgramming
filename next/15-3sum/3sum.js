aa297537435b11e8f0d9cd5c8b8318ed1fe568f3
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-12-27 16:20:14 -0800

    Time: 285 ms (18.81%) | Memory: 58.4 MB (84.84%) - LeetSync

--- next/15-3sum/3sum.js
+++ next/15-3sum/3sum.js
@@ -3,31 +3,24 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-   let result=[];
-
-   nums.sort((a,b)=>a-b)
-   for(let i=0; i<nums.length;i++){
-       let left=i+1, right=nums.length-1; 
-
-       console.log('left',left)
-       if(i>0&& nums[i]==nums[i-1])continue
-
-       while(left<right){
-           let currentSum=nums[i]+nums[left]+nums[right]
-           if(currentSum==0){
-               result.push([nums[i], nums[left], nums[right]])
-                 // Skip duplicates for the second element
+    let result=[];
+    nums.sort((a,b)=>a-b)
+    for(let i=0; i<nums.length;i++){
+        let left=i+1,right=nums.length-1
+        if(i>0&& nums[i]==nums[i-1])continue
+        while(left<right){
+            let currentSum=nums[i]+nums[left]+nums[right]
+            if(currentSum==0){
+                result.push([nums[i],nums[left],nums[right]])
+                // Skip duplicates for the second element
                 while (left < right && nums[left] === nums[left + 1])left++;
-                
                 // Skip duplicates for the third element
                 while (left < right && nums[right] === nums[right - 1]) right--;
-                
-               left++
-               right--
-           }
-           else if(currentSum<0)left++
-           else right--
-       }
-   }
- return result  
+                left++
+                right--
+            }else if(currentSum<0)left++
+            else right--
+        }
+    }
+return result
 };
