b904cf21bc32b35df21276d82a29f7698a238416
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-12-28 13:13:01 -0800

    Time: 98 ms (5.12%) | Memory: 42 MB (54.91%) - LeetSync

--- next/48-rotate-image/rotate-image.js
+++ next/48-rotate-image/rotate-image.js
@@ -3,27 +3,17 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var rotate = function(matrix) {
-//     let n = matrix.length,row=[], result=[] 
-//     for (let i = 0; i < n; i++) {
-//         for (let j = n - 1; j >= 0; j--) {
-//             row.push(matrix[j][i]);
-//         }
-//     result.push(row);
-//     }
-// return result
-
-    // Transpose the matrix
-    for (let i = 0; i < matrix.length; i++) {
-        for (let j = i; j < matrix.length; j++) {
-            // Swap matrix[i][j] with matrix[j][i]
-            let temp = matrix[i][j];
-            matrix[i][j] = matrix[j][i];
-            matrix[j][i] = temp;
+    let n=matrix.length
+    //Transpose the matrix
+    for(let i=0; i<n;i++){
+        for(j=i;j<n;j++){
+            //swap matrix[i][j] to matirx[j][i]
+            let temp=matrix[i][j]
+            matrix[i][j]=matrix[j][i]
+            matrix[j][i]=temp
         }
     }
-
-    // Reverse each row
-    for (let i = 0; i < matrix.length; i++) {
-        matrix[i].reverse();
-    }
-};
+//Reverse Each Row
+    for(let i=0; i<n;i++)matrix[i].reverse()
+    
+}
