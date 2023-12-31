5d4c7bf5e2fe205844dc96c6ae99250d05c22ae4
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-12-31 14:33:21 -0800

    Time: 94 ms (5.24%) | Memory: 42.1 MB (53.44%) - LeetSync

--- next/74-search-a-2d-matrix/search-a-2d-matrix.js
+++ next/74-search-a-2d-matrix/search-a-2d-matrix.js
@@ -4,29 +4,13 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-//Bruteforce    
-    //     let i=0
-    //     while(i<matrix.length){
-    //         for(let j=0; j<matrix[i].length; j++){
-    //             if(matrix[i][j]==target) return true    
-    //         }
-    //     i++
-    //     }
-    // return false  
-
-//Binary search
-let m= matrix.length, n=matrix[0].length,left=0, right=m*n-1
-    console.log('index',left, right)
-
-while(left<=right){
-    let mid=Math.floor((left+right)/2)
-    //Matrix is n*m or m*n 
-    //Math.floor(mid / n) calculates the row, and mid % n calculates the column
-    //mid row & mid column and check target element 
-    let mid_val = matrix[Math.floor(mid / n)][mid % n];
-    if(mid_val==target) return true
-    else if(mid_val>target) right=mid-1
-    else left=mid+1
-}
+    let m=matrix.length, n=matrix[0].length, left=0, right=m*n-1
+    while(left<=right){
+        let mid =Math.floor((left+right)/2)
+        let mid_val=matrix[Math.floor(mid/n)][mid%n]
+        if(mid_val==target)return true
+        else if(mid_val>target)right=mid-1
+        else left=mid+1
+    }
 return false
 };
