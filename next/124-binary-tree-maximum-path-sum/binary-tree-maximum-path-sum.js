3389dfc3e388c9f26bdbf6cc81ca1015f8ada07a
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-01 11:02:59 -0800

    Time: 123 ms (7.28%) | Memory: 51.3 MB (74.92%) - LeetSync

--- next/124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.js
+++ next/124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.js
@@ -11,19 +11,17 @@
  * @return {number}
  */
 var maxPathSum = function(root) {
-    let max=Number.MIN_SAFE_INTEGER
-    function findSum(root){
-        if(!root) return 0
-        let left=findSum(root.left),
-            right=findSum(root.right),
-            allSum=left+right+root.val,
-            leftNodeSum=left+root.val,
-            rightNodeSum=right+root.val
-    max=Math.max(max,root.val, allSum, leftNodeSum, rightNodeSum)
-
-    return Math.max(root.val, leftNodeSum, rightNodeSum)
-    }
-
-findSum(root)
-return max
+    max=Number.MIN_SAFE_INTEGER
+    findSum(root)
+    return max
 };
+function findSum(root){
+    if(!root)return 0
+    let left=findSum(root.left),
+        right=findSum(root.right),
+        allSum=left+right+root.val,
+        leftNodeSum=left+root.val,
+        rightNodeSum=right+root.val
+   max=Math.max(max, root.val, allSum, leftNodeSum,rightNodeSum)  
+   return Math.max(root.val, leftNodeSum, rightNodeSum)   
+}
