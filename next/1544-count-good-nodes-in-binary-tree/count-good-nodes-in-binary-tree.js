bfc07ff06e4a7ac3d37f228b68a653f87dd85d6c
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-01 10:48:30 -0800

    Time: 191 ms (7.32%) | Memory: 69.9 MB (70.38%) - LeetSync

--- next/1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.js
+++ next/1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.js
@@ -10,18 +10,17 @@
  * @param {TreeNode} root
  * @return {number}
  */
- let count=0
 var goodNodes = function(root) {
-    count = 0;
-    dfs(root, root.val);
-    return count;
-}
- function dfs(node, maxSoFar) {
-        if (!node) return;
-        if (node.val >= maxSoFar) {
-            count++;
-            maxSoFar = node.val;
-        }
-        dfs(node.left, maxSoFar);
-        dfs(node.right, maxSoFar);
+    count=0
+     dfs(root, root.val)
+ return count 
+};
+function dfs(node, maxSoFar){
+    if(!node)return
+    if(node.val>=maxSoFar){
+        count++
+        maxSoFar=node.val
     }
+    dfs(node.left,maxSoFar)
+    dfs(node.right,maxSoFar)
+}
