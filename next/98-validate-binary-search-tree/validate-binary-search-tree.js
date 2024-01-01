a4d37d840b602ef138994342bb907078b2812caa
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-01 11:34:32 -0800

    Time: 67 ms (33.13%) | Memory: 46.8 MB (24.00%) - LeetSync

--- next/98-validate-binary-search-tree/validate-binary-search-tree.js
+++ next/98-validate-binary-search-tree/validate-binary-search-tree.js
@@ -10,12 +10,15 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isValidBST = function(root, min = Number.MIN_SAFE_INTEGER, max = Number.MAX_SAFE_INTEGER) {
-    if (!root) return true;
-
-    if (root.val <= min || root.val >= max) return false;
-
-    // For the left subtree, update the max constraint to root.val
-    // For the right subtree, update the min constraint to root.val
-    return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+var isValidBST = function(root) {
+   function dfs(root,left,right){
+        if (!root) {
+            return true
+        }
+        if (root.val <=left || root.val >= right) {
+            return false
+        }
+        return dfs(root.left,left,root.val) && dfs(root.right,root.val,right)
+    }
+    return dfs(root,-Infinity,Infinity)
 };
