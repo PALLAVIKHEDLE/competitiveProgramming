8cd762096facd04256f92f7d218795968e0500c9
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2023-12-31 17:26:32 -0800

    Time: 87 ms (44.63%) | Memory: 48.6 MB (33.60%) - LeetSync

--- next/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ next/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -2,45 +2,18 @@
  * @param {string} s
  * @return {number}
  */
-
-
 var lengthOfLongestSubstring = function(s) {
-    let map = {}, start=end =max=0;
-    while (end < s.length) {
-        // If the character at the end index is not present in the map,
-        //If the character at the end index is already present in the map and has a count of 0
-        if (map[s[end]] === undefined || map[s[end]] === 0) {
-            map[s[end]] = 1;
-            end++;
-            max = Math.max(max, end - start);
-        } else {
-            map[s[start]] = 0;  // Reset the count for the character at start index
-            start++;
+    let right=s.length, map={},start=max=left=0;
+    while(left<right){
+        if(map[s[left]]===undefined||map[s[left]]===0){
+            map[s[left]]=1
+            left++
+            max=Math.max(max,left-start)
+        }    
+        else {
+            map[s[start]]=0
+            start++
         }
     }
-
-    return max;  
-
-//      let map={}, currentWindowLength, maxLength=0
-     
-//     for(let i=0; i<s.length;i++){
-//         let val=map[s[i]]
-//         if(val==undefined){
-//             map[s[i]]=i
-//             currentWindowLength=Object.keys(map).length//size of map
-//             maxLength=Math.max(maxLength,currentWindowLength)
-//         }else{
-//             resetMap(map,val)
-//             map[s[i]]=i
-//         }
-//     }
-//   return maxLength  
+    return max
 };
-
-
-// function resetMap(map,val){
-//     for(key in map){
-//         // console.log(map[key],val)
-//         if(map[key]<=val)delete map[key]
-//     }
-// }
