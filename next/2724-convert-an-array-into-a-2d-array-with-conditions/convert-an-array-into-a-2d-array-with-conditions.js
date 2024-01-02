aa29bd3cf5b3048add036a8f2a73a4da04aa76ca
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-01 19:41:05 -0800

    Time: 155 ms (15.46%) | Memory: 45.9 MB (80.41%) - LeetSync

--- next/2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.js
+++ next/2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.js
@@ -3,19 +3,33 @@
  * @return {number[][]}
  */
 var findMatrix = function(nums) {
-   let map = {},max = 0
+//    let map = {},max = 0
+
+//     for (const num of nums) {
+//        if(!map[num])map[num]=1
+//        else map[num]+=1
+//       max = Math.max(max, map[num])
+//     }
+
+//     // Create an array of arrays ('result') to store the final 2D array
+//     let result = Array.from({ length: max }, () => []);
+
+//     for (let key in map) {
+//         for (let i = 0; i < map[key]; i++)result[i].push(key)
+//     }
+//     return result;
+ let result = [];
 
     for (const num of nums) {
-       if(!map[num])map[num]=1
-       else map[num]+=1
-      max = Math.max(max, map[num])
-    }
-
-    // Create an array of arrays ('result') to store the final 2D array
-    let result = Array.from({ length: max }, () => []);
-
-    for (let key in map) {
-        for (let i = 0; i < map[key]; i++)result[i].push(key)
+        let present = false  
+        for (const row of result) {
+            if (!row.includes(num)) {
+                row.push(num);
+                present = true;
+                break;
+            }
+        }
+        if(!present)result.push([num]);
     }
     return result;
 };
