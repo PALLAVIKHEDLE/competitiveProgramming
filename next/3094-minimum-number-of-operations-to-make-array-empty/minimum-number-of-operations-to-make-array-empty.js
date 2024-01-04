0c0f77ecc412793ccaf74a2907d25645282278d8
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-03 16:35:04 -0800

    Time: 136 ms (48.72%) | Memory: 55.6 MB (28.21%) - LeetSync

--- next/3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.js
+++ next/3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.js
@@ -8,10 +8,8 @@ var minOperations = function(nums) {
         if(map[num]==undefined)map[num]=1
         else map[num]=map[num]+1
     }
-    // for (let [key, value] of Object.entries(map)) {
     for (key in map) {
         let freq=map[key]
-        // let freq = value;
         if (freq === 1) return -1;//If the frequency is 1, return -1 
         count += Math.floor(freq / 3);// For every group of three, perform one operation.
         if (freq % 3 !== 0) count++;//If there are remaining elements (1 or 2), perform additional operations.
