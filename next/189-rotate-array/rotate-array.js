bac47acd8258c392b58b46e36d5f51c3af0bc625
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-05 11:30:48 -0800

    Time: 91 ms (26.77%) | Memory: 51.1 MB (82.96%) - LeetSync

---
+++ next/189-rotate-array/rotate-array.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    if(!nums)return
+    k=k%nums.length
+    if(k==0)return
+    const reverse=((start,end)=>{
+        while(start<=end){
+            [nums[end],nums[start]]=[nums[start],nums[end]]
+            start++
+            end--
+        }
+    })
+    reverse(0,nums.length-1)
+    reverse(0,k-1)
+    reverse(k,nums.length-1)
+
+
+
+};
