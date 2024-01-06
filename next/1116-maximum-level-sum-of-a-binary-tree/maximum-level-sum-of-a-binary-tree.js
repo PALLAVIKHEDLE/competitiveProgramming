18daca94309ac6c4cb7e4836c149c43ba3907950
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-06 15:28:16 -0800

    Time: 137 ms (39.69%) | Memory: 66.3 MB (98.59%) - LeetSync

--- next/1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.js
+++ next/1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.js
@@ -11,30 +11,28 @@
  * @return {number}
  */
 var maxLevelSum = function(root) {
-      if (!root) return 0
+    if (!root) return 0
 
-    let level = 1
+    let level = 0
     let res = { level: 1, sum: root.val } 
 
     const queue = [root]
 
     while (queue.length) {
-        const queueLen = queue.length 
-
+        level++
+        let queueLen = queue.length 
         let newSum = 0 
 
-        for (let i = 0; i < queueLen; i++) {
+        while(queueLen--) {
             const node = queue.shift()
             newSum += node.val 
             if (node.left) queue.push(node.left)
             if (node.right) queue.push(node.right)
         }
 
-        if (newSum > res.sum) {
-            res = { level, sum: newSum}
-        }
-
-        level++
+        if (newSum > res.sum)res = { level, sum: newSum}
+        
+        // level++
     }
 
     return res.level
