3d3b99cefc15231ad06dfc2ce6fe769be68d0e83
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-07 18:43:35 -0800

    Time: 112 ms (91.14%) | Memory: 96.4 MB (67.72%) - LeetSync

--- next/975-range-sum-of-bst/range-sum-of-bst.js
+++ next/975-range-sum-of-bst/range-sum-of-bst.js
@@ -13,20 +13,30 @@
  * @return {number}
  */
 var rangeSumBST = function(root, low, high) {
-    let result=[], sum=0
-    if(!root)return null
+    // let result=[], sum=0
+    // if(!root)return null
    
-        const inOrderTraversal = function(node) {
-        if (node) {
-            inOrderTraversal(node.left);
-            result.push(node.val);
-            inOrderTraversal(node.right);
-        }
-    };
+    //     const inOrderTraversal = function(node) {
+    //     if (node) {
+    //         inOrderTraversal(node.left);
+    //         result.push(node.val);
+    //         inOrderTraversal(node.right);
+    //     }
+    // };
 
-    inOrderTraversal(root);
-    for(let i=0; i<result.length;i++){
-        if(low<=result[i] && result[i]<=high)sum+=result[i]
-    }
+    // inOrderTraversal(root);
+    // for(let i=0; i<result.length;i++){
+    //     if(low<=result[i] && result[i]<=high)sum+=result[i]
+    // }
+    // return sum
+     if (!root) return 0
+    let sum = 0
+
+    if (low <= root.val && root.val <= high) sum += root.val
+
+    if (root.left) sum += rangeSumBST(root.left, low, high)
+    if (root.right) sum += rangeSumBST(root.right, low, high)
+
+
     return sum
 };
