6305d77cd9f8ab1d714784c740f1a4ee4b309762
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-07 18:06:49 -0800

    Time: 49 ms (73.06%) | Memory: 41.9 MB (38.37%) - LeetSync

---
+++ next/394-decode-string/decode-string.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var decodeString = function(s) {
+    let stack=[]
+    for(let str of s){
+        if(str!==']')stack.push(str)
+        else{
+            let currStr=''
+            //grab the char until we see a '['
+            while(stack.length&& stack[stack.length-1]!=='['){
+                currStr=stack.pop()+currStr
+            }
+            stack.pop()//remove'['
+            let nums=''
+            //garb the numbers until we see a letter
+            while(stack.length&&!isNaN( stack[stack.length-1])){
+                nums=stack.pop()+nums// reverse 
+            }
+
+            nums=parseInt(nums)
+             let decodedStr = '';
+            for (let i = 0; i <nums; i++) {
+                decodedStr += currStr;
+            }
+            
+            stack.push(decodedStr);
+        }
+    }
+    return stack.join('');
+};
