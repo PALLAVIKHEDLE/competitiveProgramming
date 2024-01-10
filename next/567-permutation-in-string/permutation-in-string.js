7906efe5651d0f1cdccfb9d0349a1d36c16fad93
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-10 15:47:49 -0800

    Time: 75 ms (66.93%) | Memory: 44 MB (71.50%) - LeetSync

---
+++ next/567-permutation-in-string/permutation-in-string.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var checkInclusion = function(s1, s2) {
+    let {length}=s1, start=end=0, map={}
+    if(s1.length>s2.length)return false
+    for(str of s1){
+        if(map[str]==undefined)map[str]=1
+        else map[str]++
+    }
+    while(end<s2.length){
+        if(map[s2[end]]>0)length--
+        map[s2[end]]--
+        end++
+        if(length==0)return true
+        if(end-start==s1.length){
+            if(map[s2[start]]>=0)length++
+            map[s2[start]]++
+            start++
+        }
+    }
+    return false
+};
