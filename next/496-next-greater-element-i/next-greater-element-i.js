bb2dc427318fad8beb0e171f8be866c7016ca8b0
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-09 20:34:22 -0800

    Time: 62 ms (42.27%) | Memory: 42.2 MB (97.04%) - LeetSync

--- next/496-next-greater-element-i/next-greater-element-i.js
+++ next/496-next-greater-element-i/next-greater-element-i.js
@@ -4,22 +4,22 @@
  * @return {number[]}
  */
 var nextGreaterElement = function(nums1, nums2) {
-    
-const map={}; let finalArray=[]
-for(let i=0; i<nums2.length;i++){
-  let greater=-1
-  for(let j=i+1;j<nums2.length;j++){
-    if(nums2[j]>nums2[i]){
-      greater=nums2[j]
-      break
+    let blankArr = []
+    for(let i = 0; i < nums1.length; i++){
+        let max = -1;
+        for(let j = 0; j < nums2.length; j++){
+            let index = j;
+            if(nums1[i] == nums2[j]){ 
+                while(index !== nums2.length){ 
+                    if(nums2[index] > nums1[i]){
+                        max = nums2[index]; 
+                        break;
+                    }
+                    index++;
+                }
+            }
+        }
+        blankArr.push(max);
     }
-  }
-  map[nums2[i]]=greater
-}
-  for(let i=0; i<nums1.length; i++){
-    for(key in map){
-        if(key==nums1[i])finalArray.push(map[key])
-    }
-  }
-  return finalArray
+        return blankArr;
 };
