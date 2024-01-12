bee32c68e3d8e6cbc5037353d107cff035889b34
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-11 18:36:57 -0800

    Time: 60 ms (52.98%) | Memory: 44.9 MB (15.44%) - LeetSync

--- next/1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.js
+++ next/1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.js
@@ -4,7 +4,7 @@
  */
 var halvesAreAlike = function(s) {
   let vowels = ['a', 'e', 'i', 'o', 'u'];
-    let mid = Math.floor(s.length / 2), fCount = 0, sCount = 0;
+mid = Math.floor(s.length / 2), fCount = 0, sCount = 0;
 
     for (let i = 0; i < mid; i++) {
         if (vowels.includes(s[i].toLowerCase())) fCount++;
