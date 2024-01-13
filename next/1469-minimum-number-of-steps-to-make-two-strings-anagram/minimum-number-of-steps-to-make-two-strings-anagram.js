6d488ca5897752c9bb73695259b73021d00224a6
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-12 17:05:56 -0800

    Time: 126 ms (66.86%) | Memory: 48.7 MB (28.49%) - LeetSync

---
+++ next/1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
+var minSteps = function(s, t) {
+    let count=0
+    let map1={}, map2={}
+    for(let char of s){
+        map1[char]==undefined?map1[char]=1:map1[char]++
+    }
+
+    for(let char of t){
+        map2[char]==undefined?map2[char]=1:map2[char]++
+    }
+
+     for (let key in map2) {
+        count += Math.max(0, map2[key] - (map1[key] || 0));
+    }
+    return count
+};
