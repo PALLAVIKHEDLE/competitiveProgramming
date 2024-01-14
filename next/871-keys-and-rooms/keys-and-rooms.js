a39f7f6cc67bd68786f974af4fc60b6b81dbd916
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-13 17:48:15 -0800

    Time: 54 ms (72.16%) | Memory: 44.5 MB (26.72%) - LeetSync

--- next/871-keys-and-rooms/keys-and-rooms.js
+++ next/871-keys-and-rooms/keys-and-rooms.js
@@ -2,33 +2,33 @@
  * @param {number[][]} rooms
  * @return {boolean}
  */
-// var canVisitAllRooms = function(rooms) {
-//       let map = {};//map will keep track of visited rooms.
-
-//     function dfs(room) {
-//         //If the room is not visited, marks the room as visited by setting its value to true 
-//         if (map[room] === undefined)  map[room] = true;
-//         for (let i of rooms[room]) if (!map[i]) dfs(i);
-//     }
-
-//     dfs(0);//start from room 0
-
-// // if the number of keys in the map object is equal to the total number of rooms
-//     return Object.keys(map).length === rooms.length;
-// };
 var canVisitAllRooms = function(rooms) {
- const visited = new Set();
+      let map = {};//map will keep track of visited rooms.
 
-    function visit(i) {
-        visited.add(i);
-        const keys = rooms[i];
-        for (const key of keys) {
-            if (!visited.has(key)) {
-                visit(key);
-            }
-        }
+    function dfs(room) {
+        //If the room is not visited, marks the room as visited by setting its value to true 
+        if (map[room] === undefined)  map[room] = true;
+        for (let i of rooms[room]) if (!map[i]) dfs(i);
     }
-    visit(0);
 
-    return visited.size === rooms.length;
-}
+    dfs(0);//start from room 0
+
+// if the number of keys in the map object is equal to the total number of rooms
+    return Object.keys(map).length === rooms.length;
+};
+// var canVisitAllRooms = function(rooms) {
+//  const visited = new Set();
+
+//     function visit(i) {
+//         visited.add(i);
+//         const keys = rooms[i];
+//         for (const key of keys) {
+//             if (!visited.has(key)) {
+//                 visit(key);
+//             }
+//         }
+//     }
+//     visit(0);
+
+//     return visited.size === rooms.length;
+// }
