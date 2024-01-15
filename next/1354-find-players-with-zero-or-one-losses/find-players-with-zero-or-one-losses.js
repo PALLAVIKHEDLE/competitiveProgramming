d462a77ab2ce2f8553f90239c239a7ad2883cb3e
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-15 10:21:50 -0800

    Time: 373 ms (31.78%) | Memory: 132.9 MB (17.13%) - LeetSync

--- next/1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.js
+++ next/1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.js
@@ -3,18 +3,24 @@
  * @return {number[][]}
  */
 var findWinners = function(matches) {
-    let map={}
+ let map={}// store the count of losses for each player
 
-    for(let match of matches){
-        if(map[match[0]]==undefined)map[match[0]]=0
-        if(map[match[1]]==undefined)map[match[1]]=0
-        map[match[1]]++
-    }
+ for(let match of matches){
+     if(map[match[0]]==undefined)map[match[0]]=0//Initialize losses for player1
+     if(map[match[1]]==undefined)map[match[1]]=0//initialize losses for player2
 
-    let ans=[[],[]]
-    for(let [key,value] of Object.entries(map)){
-        if(value === 0)ans[0].push(key)
-        if(value === 1)ans[1].push(key)
+     if (match[0] !== match[1]) {
+      // increment losses only if players are different 
+      map[match[1]]++; // increment losses for player 2
     }
-    return ans
+ }
+ let ans = [[], []];
+
+ for(let [key, value] of Object.entries(map)){
+     // count of losses is 0
+     if(value==0)ans[0].push(parseInt(key))
+     // count of losses is 1
+     if (value === 1) ans[1].push(parseInt(key));
+ }
+ return ans
 };
