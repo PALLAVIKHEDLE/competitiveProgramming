d863595f217a8c712466e7f5ea3dc020a5db97c8
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-15 14:18:06 -0800

    Time: 77 ms (86.38%) | Memory: 51.2 MB (13.23%) - LeetSync

--- next/450-delete-node-in-a-bst/delete-node-in-a-bst.js
+++ next/450-delete-node-in-a-bst/delete-node-in-a-bst.js
@@ -12,22 +12,23 @@
  * @return {TreeNode}
  */
 var deleteNode = function(root, key) {
-     if(!root) return null
-     if(root.val===key){
-         if(!root.left &&!root.right) return null
-         if(!root.left)return root.right
-         if(!root.right)return root.left
-         if(root.left && root.right){
-             let curr=root.right
-             while(curr.left){
-                 curr=curr.left
-             }
-             curr.left=root.left
-             return root.right
-         }
-     }
-     if(key<root.val)root.left= deleteNode(root.left,key)//look for our target node in the left subtree
-     else root.right= deleteNode(root.right,key)// look for our target node in the right subtree
+    if(!root)return null
+    if(root.val==key){
+        if(!root.left && !root.right)return null
+        if(!root.left)return root.right
+        if(!root.right)return root.left
+        if(root.left && root.right){
+            let curr=root.right
+            while(curr.left){
+                curr=curr.left
+            }
+            curr.left=root.left
+            return root.right
+        }
+    }
+    if(key<root.val)root.left=deleteNode(root.left,key)
+    else root.right= deleteNode(root.right,key)
 
-return root     
+
+return root    
 };
