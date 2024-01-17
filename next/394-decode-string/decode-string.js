53a850342efe57c3459bd6085c2faedddee07120
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-17 15:13:32 -0800

    Time: 55 ms (37.45%) | Memory: 42.1 MB (27.83%) - LeetSync

--- next/394-decode-string/decode-string.js
+++ next/394-decode-string/decode-string.js
@@ -4,29 +4,21 @@
  */
 var decodeString = function(s) {
   let stack=[]
-    for(let str of s){
-        if(str!==']')stack.push(str)
-        else{
-            let currStr=''
-            //grab the char until we see a '['
-            while(stack.length&& stack[stack.length-1]!=='['){
-                currStr=stack.pop()+currStr
-            }
-            stack.pop()//remove'['
-            let nums=''
-            //garb the numbers until we see a letter
-            while(stack.length&&!isNaN( stack[stack.length-1])){
-                nums=stack.pop()+nums// reverse 
-            }
+  for(let str of s){
+      if(str!==']')stack.push(str)
+      else{
+          let currStr=''
+          while(stack.length&& stack[stack.length-1]!=='[')currStr=stack.pop()+currStr
+            stack.pop()
+          let num=''
+          while(stack.length&& !isNaN(stack[stack.length-1]))num=stack.pop()+num
 
-            nums=parseInt(nums)
-             let decodedStr = '';
-            for (let i = 0; i <nums; i++) {
-                decodedStr += currStr;
-            }
-            
-            stack.push(decodedStr);
-        }
-    }
-    return stack.join('');
+          num=parseInt(num)
+          let decodeStr=''
+          for(let i=0; i<num;i++) decodeStr+=currStr  
+
+          stack.push(decodeStr)
+      }
+  }
+  return stack.join('')
 };
