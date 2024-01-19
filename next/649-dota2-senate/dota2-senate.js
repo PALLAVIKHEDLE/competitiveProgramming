3d02d6c75c5b81d956b9dbff920082a7f901055c
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-19 12:24:46 -0800

    Time: 69 ms (62.97%) | Memory: 51.3 MB (5.06%) - LeetSync

--- next/649-dota2-senate/dota2-senate.js
+++ next/649-dota2-senate/dota2-senate.js
@@ -3,46 +3,17 @@
  * @return {string}
  */
 var predictPartyVictory = function(senate) {
-    
-    //Queue Implementation
-    let votingQueue=new Queue()
-    let dCount=0; //remaining "Dire" senators
-    let rCount=0; //remaining "Radiant" senators
-    let dBanCount=0; //How many "Dire" that need to be banned
-    let rBanCount=0; //How many "Radiant" that need to be banned
+    //Stack Implementation
+    let dire=[],rad=[],n=senate.length
 
-    //put them all in a voting queue and count them to start
-    for(let i=0; i<senate.length; i++){
-        let voter=senate[i]
-        votingQueue.enqueue(voter)
-        if(voter=='R')rCount++
-        else dCount++
+    for(let i=0; i<n;i++){
+        senate[i]==='R'?rad.push(i):dire.push(i)
     }
-
-    //Process the voter until one side is eliminated
-    while(votingQueue.size()>0 && dCount>0 && rCount>0){
-        let nextVote=votingQueue.dequeue()
-        if(nextVote=='R'){
-            if(rBanCount>0){
-                rBanCount--
-                rCount--//senator is gone
-            }else{
-                dBanCount++//vote off next "Dire" senator
-                votingQueue.enqueue(nextVote)
-            }
-        }else{
-            if(dBanCount>0){
-                dBanCount--
-                dCount--//dire is gone
-            }else{
-                rBanCount++//vote off next "Radiant" senator
-                votingQueue.enqueue(nextVote)
-            }
-        }
+    while(rad.length>0 && dire.length>0){
+        const rSenator=rad.shift()
+        const dSenator=dire.shift()
+        if(rSenator<dSenator)rad.push(n++)
+        else dire.push(n++)
     }
-    // If "Dire" is eliminated return "Radiant" otherwise "Dire" won!
-    return dCount == 0 ? "Radiant" : "Dire";
+    return rad.length==0?'Dire':'Radiant'
 };
-
-
-  
