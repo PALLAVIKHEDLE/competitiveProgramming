e881a9d1c3f50049d692b3721533089b4c412983
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-19 09:54:32 -0800

    Time: 156 ms (24.79%) | Memory: 73.4 MB (100.00%) - LeetSync

--- next/1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.js
+++ next/1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.js
@@ -11,17 +11,18 @@
  * @return {number}
  */
 var longestZigZag = function(root) {
-  let max=0
+     let max=0
 
-  const dfs=(node, prev, length)=>{
-      if(!node)return
-      max=Math.max(max,length)
+    const dfs=(node, prev,length)=>{
+        if(!node) return 
+        max=Math.max(max,length)
 
-      dfs(node.left,'left', prev==='right'?length+1:1)
-      dfs(node.right,'right', prev==='left'?length+1:1)
+        dfs(node.left, 'left' ,prev=='right'?length+1:1)
+        dfs(node.right,'right', prev=='left'?length+1:1)
+    }
 
-  }
-  dfs(root, 'left',0)  
-  dfs(root, 'right',0)  
-return max
+dfs(root, 'left',0)
+dfs(root, 'right',0)
+
+return max  
 };
