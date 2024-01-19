178e11222813f06eecd8a659d07982fffe7c47ec
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-18 22:23:53 -0800

    Time: 61 ms (56.69%) | Memory: 44.3 MB (54.14%) - LeetSync

--- next/967-minimum-falling-path-sum/minimum-falling-path-sum.js
+++ next/967-minimum-falling-path-sum/minimum-falling-path-sum.js
@@ -3,18 +3,16 @@
  * @return {number}
  */
 var minFallingPathSum = function(matrix) {
-    const row = matrix.length;
-    const col = matrix[0].length;
+   let row=matrix.length, col=matrix[0].length;
 
-    for (let i = 1; i < row; i++) {
-        for (let j = 0; j < col; j++) {
-            matrix[i][j] += Math.min(
-                matrix[i - 1][j],
-                (j > 0) ? matrix[i - 1][j - 1] : Infinity,
-                (j < col - 1) ? matrix[i - 1][j + 1] : Infinity
-            );
+    for(let i=1; i<row; i++){
+        for(let j=0; j<col;j++){
+            matrix[i][j]+=Math.min(
+                matrix[i-1][j], //element directly above
+                (j>0)?matrix[i-1][j-1]:Infinity,//element diagonally left 
+                (j<col-1)?matrix[i-1][j+1]:Infinity//element diagonally right
+            )
         }
     }
-
-    return Math.min(...matrix[row - 1]);
+ return Math.min(...matrix[row-1]) //minimum value in the last row,  
 };
