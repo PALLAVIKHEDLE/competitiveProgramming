090d535dcc456dd48f47c387cc1bde425409f186
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-19 09:44:13 -0800

    Time: 68 ms (13.92%) | Memory: 50.9 MB (5.99%) - LeetSync

--- next/103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.js
+++ next/103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.js
@@ -11,19 +11,19 @@
  * @return {number[][]}
  */
 var zigzagLevelOrder = function(root) {
-    if(!root) return []
+    if(!root)return []
     let queue=[root], result=[], count=0
     while(queue.length){
         let len=queue.length
         if(count%2==0)result.push(queue.map(node=>node.val))
         else result.push(queue.map(node=>node.val).reverse())
-        count++
 
+        count++
         while(len--){
             let curr=queue.shift()
             if(curr.left)queue.push(curr.left)
             if(curr.right)queue.push(curr.right)
         }
     }
-    return result
+return result
 };
