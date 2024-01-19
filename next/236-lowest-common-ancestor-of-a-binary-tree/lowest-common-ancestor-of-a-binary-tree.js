3c84e45bed8b058ba5e06fade4960c0c8bee42e6
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-19 10:44:23 -0800

    Time: 63 ms (84.84%) | Memory: 58.6 MB (5.09%) - LeetSync

--- next/236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.js
+++ next/236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.js
@@ -12,12 +12,26 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
-    if(!root|| root==p|| root==q)return root
-    let left=lowestCommonAncestor(root.left,p,q)
-    let right=lowestCommonAncestor(root.right,p,q)
+    const dfs=(node)=>{
+        if(!node)return null
+        if(node==p||node==q) return node
+        const left=dfs(node.left)
+        const right=dfs(node.right)
 
-    if (!left) return right  // p and q are in the right subtree
-    if (!right) return left  // p and q are in the left subtree
-    return root              // p is in one side and q is in the other
+        if(!left)return right
+        if(!right)return left
+        return node
+        
+    }
     
+  
+    return dfs(root)
+
+      // if(!root|| root==p|| root==q)return root
+    // let left=lowestCommonAncestor(root.left,p,q)
+    // let right=lowestCommonAncestor(root.right,p,q)
+
+    // if (!left) return right  // p and q are in the right subtree
+    // if (!right) return left  // p and q are in the left subtree
+    // return root              // p is in one side and q is in the other
 };
