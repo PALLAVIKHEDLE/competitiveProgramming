fe89e02eafc806e212a9e8d7b39dce28afd10e72
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-20 13:11:01 -0800

    Time: 69 ms (95.00%) | Memory: 55 MB (7.24%) - LeetSync

--- next/23-merge-k-sorted-lists/merge-k-sorted-lists.js
+++ next/23-merge-k-sorted-lists/merge-k-sorted-lists.js
@@ -9,37 +9,32 @@
  * @param {ListNode[]} lists
  * @return {ListNode}
  */
-function mergeLists(l1,l2){
-    const dummy= new ListNode(0)
-    let current=dummy;
-    if(!l1 ||!l2) return l1?l1:l2
-    while(l1&&l2){
-        if(l1.val<l2.val){
-            current.next=l1
-            l1=l1.next
-        }else{
-            current.next=l2
-            l2=l2.next
-        }
-        current=current.next
-    }
+ function mergeLists(l1,l2){
+     const dummy=new ListNode(0)
+     let current=dummy
+     if(!l1 || !l2) return l1?l1:l2
+
+     while(l1 && l2){
+         if(l1.val<l2.val){
+             current.next=l1
+             l1=l1.next
+         }else{
+             current.next=l2
+             l2=l2.next
+         }
+         current=current.next
+     }
      current.next=l1||l2
-return dummy.next
-}
-
+  return dummy.next   
+ }
 var mergeKLists = function(lists) {
-    if(lists.length===0) return null
-    
-    while(lists.length>1){
-        let l1=lists.shift();
-        let l2=lists.shift();
-        const h=mergeLists(l1,l2)
-        console.log('h',h)
-         lists.push(h);
-    }
-    console.log('lst',lists)
-  return lists[0];  
+  if(lists.length==0)return null
+  //it takes 2 list at a time, loop continues until there is only one list left
+  while(lists.length>1){
+      let l1=lists.shift()
+      let l2=lists.shift()
+      const val=mergeLists(l1,l2)
+      lists.push(val)
+  } 
+  return lists[0] 
 };
-
-
-
