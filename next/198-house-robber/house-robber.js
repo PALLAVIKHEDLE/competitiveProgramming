83ad66461d344a10fa237aee1cd9b136fcd61ce9
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-20 20:07:53 -0800

    Time: 54 ms (46.38%) | Memory: 48.4 MB (5.18%) - LeetSync

--- next/198-house-robber/house-robber.js
+++ next/198-house-robber/house-robber.js
@@ -3,11 +3,10 @@
  * @return {number}
  */
 var rob = function(nums) {
-      let evenSum = 0; oddSum = 0;
-
-    for (let i = 0; i < nums.length; i++) {
-        if (i % 2 === 0) evenSum = Math.max(evenSum + nums[i], oddSum);
-        else oddSum = Math.max(oddSum + nums[i], evenSum);  
+    let evenSum=oddSum=0
+    for(let i=0; i<nums.length;i++){
+        if(i%2==0)evenSum=Math.max(evenSum+nums[i],oddSum)
+        else oddSum=Math.max(oddSum+nums[i],evenSum)
     }
-    return Math.max(evenSum, oddSum);
+    return Math.max(evenSum, oddSum)
 };
