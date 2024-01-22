77bba7d2311333b071475290a1edda031f79c378
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-22 14:27:29 -0800

    Time: 74 ms (81.57%) | Memory: 56 MB (7.68%) - LeetSync

--- next/658-find-k-closest-elements/find-k-closest-elements.js
+++ next/658-find-k-closest-elements/find-k-closest-elements.js
@@ -5,15 +5,21 @@
  * @return {number[]}
  */
 var findClosestElements = function(arr, k, x) {
-    let result=[], diffsWithIndices=[]
-    for(let i=0; i<arr.length;i++){
-        let diff=Math.abs(x-arr[i])
-        diffsWithIndices.push({diff, index:i})
+    let left=0, right=arr.length-1
+    while(right-left>=k){
+        if(Math.abs(arr[left]-x)<=Math.abs(arr[right]-x))right--
+        else left++
     }
-    diffsWithIndices.sort((a,b)=>a.diff-b.diff)
+    return arr.slice(left, right+1)
+//     let result=[], diffsWithIndices=[]
+//     for(let i=0; i<arr.length;i++){
+//         let diff=Math.abs(x-arr[i])
+//         diffsWithIndices.push({diff, index:i})
+//     }
+//     diffsWithIndices.sort((a,b)=>a.diff-b.diff)
 
-    for(let j=0; j<k;j++)result.push(arr[diffsWithIndices[j].index])
-    result.sort((a,b)=>a-b)
+//     for(let j=0; j<k;j++)result.push(arr[diffsWithIndices[j].index])
+//     result.sort((a,b)=>a-b)
 
-  return result  
+//   return result  
 };
