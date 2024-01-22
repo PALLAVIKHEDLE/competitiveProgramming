6ec20845d502c074a285aa39e430e7b5092eabe5
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-21 22:24:07 -0800

    Time: 70 ms (11.11%) | Memory: 48.9 MB (5.17%) - LeetSync

--- next/74-search-a-2d-matrix/search-a-2d-matrix.js
+++ next/74-search-a-2d-matrix/search-a-2d-matrix.js
@@ -1,16 +1,16 @@
 /**
  * @param {number[][]} matrix
- * @param {number} target
+ * @param {number} targetkya bnau
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-    let m=matrix.length, n=matrix[0].length, left=0, right=m*n-1
+    let row=matrix.length, column=matrix[0].length, left=0, right=row*column-1
     while(left<=right){
-        let mid =Math.floor((left+right)/2)
-        let mid_val=matrix[Math.floor(mid/n)][mid%n]
+        let mid=Math.floor((left+right)/2)
+        let mid_val=matrix[Math.floor(mid/column)][mid%column]
         if(mid_val==target)return true
         else if(mid_val>target)right=mid-1
         else left=mid+1
     }
-return false
+return false    
 };
