2b8276dad73f6f443492f154fbe97b9c2828de74
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-22 09:39:44 -0800

    Time: 108 ms (18.67%) | Memory: 56.4 MB (5.05%) - LeetSync

--- next/2-add-two-numbers/add-two-numbers.js
+++ next/2-add-two-numbers/add-two-numbers.js
@@ -11,16 +11,15 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    let dummyHead=new ListNode(0);
-    let current=dummyHead, carry=0
-
-    while(l1|| l2 || carry){
-        let sum=(l1?l1.val:0)+(l2?l2.val:0)+carry
-        carry=Math.floor(sum/10)
-        current.next=new ListNode(sum%10)
-        current=current.next
-        if(l1)l1=l1.next
-        if(l2)l2=l2.next
-    }
-return dummyHead.next    
+       let dummyHead=new ListNode(0)
+       let current=dummyHead, carry=0
+       while(l1||l2||carry){
+           let sum=(l1?l1.val:0)+(l2?l2.val:0)+carry
+           carry=Math.floor(sum/10)
+           current.next=new ListNode(sum%10)
+           current=current.next
+           if(l1)l1=l1.next
+           if(l2)l2=l2.next
+       }
+return dummyHead.next
 };
