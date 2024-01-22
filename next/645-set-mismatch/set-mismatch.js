4a7e42ff7a7aba936f0c1217070615ac9d7d9d23
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-21 21:47:58 -0800

    Time: 78 ms (36.72%) | Memory: 57.1 MB (5.25%) - LeetSync

--- next/645-set-mismatch/set-mismatch.js
+++ next/645-set-mismatch/set-mismatch.js
@@ -3,12 +3,19 @@
  * @return {number[]}
  */
 var findErrorNums = function(nums) {
-     let result = [];
-    
-    nums = nums.sort()
-    for(let i=1; i <= nums.length; i++){
-       if(nums[i - 1] == nums[i]) result[0] = nums[i]
-       if(nums.indexOf(i) == -1) result[1] = i;
-    }
-    return result
+     let map={}, result=[]
+     for(let num of nums){
+         map[num]=(map[num]||0)+1;
+     }
+     //find the repeated & mising number 
+     for(let key in map){
+         if(map[key]>1)result.push(key)//repeated number
+     }
+     for(let i=1; i<=nums.length;i++){
+         if(!map[i]){
+             result.push(i)//missing number
+             break//break once the missing number is found
+         }
+     }
+return result     
 };
