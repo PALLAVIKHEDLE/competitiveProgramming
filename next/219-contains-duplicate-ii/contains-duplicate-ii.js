6088d8c281e8e702369ccd4f21cc111291390ab5
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-22 16:04:01 -0800

    Time: 128 ms (24.34%) | Memory: 69.3 MB (15.54%) - LeetSync

---
+++ next/219-contains-duplicate-ii/contains-duplicate-ii.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    let map={}
+    for(let i=0; i<nums.length;i++){
+        if(map[nums[i]]==undefined)map[nums[i]]=i
+        else{
+           let diff=map[nums[i]]-i
+
+            if(Math.abs(diff)<=k)return true
+            else map[nums[i]]=[i]
+        }
+    }
+return false
+};
