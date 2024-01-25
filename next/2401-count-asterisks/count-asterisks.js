56d4a0349fb06b5d56592b1951785176254c6c9e
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-25 00:16:57 -0800

    Time: 50 ms (79.20%) | Memory: 49.3 MB (5.11%) - LeetSync

--- next/2401-count-asterisks/count-asterisks.js
+++ next/2401-count-asterisks/count-asterisks.js
@@ -3,19 +3,13 @@
  * @return {number}
  */
 var countAsterisks = function(s) {
-//     let barCount=0, count=0
-//     for(let str of s){
-//         if(str=='|')barCount++
-//         if(barCount%2==0){
-//             if(str=='*')count++
-//         }
-//     }
-//  return count   
- let notPair = true;
-    let cnt = 0;
-    for (const c of s) {
-        if (c === '|')  notPair = !notPair;
-         else if (notPair && c === '*') cnt++;
+    let barCount=0, count=0
+    for(let str of s){
+        if(str=='|')barCount++
+
+        if(barCount%2==0){
+            if(str=='*')count++
+        }
     }
-    return cnt;
+    return count
 };
