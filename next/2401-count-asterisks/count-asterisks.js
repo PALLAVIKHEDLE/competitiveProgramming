29e835f468e4e04c0b28613841cdfee45c7fbbec
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-24 23:55:59 -0800

    Time: 63 ms (20.07%) | Memory: 50.7 MB (5.11%) - LeetSync

---
+++ next/2401-count-asterisks/count-asterisks.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countAsterisks = function(s) {
+    let barCount=0, count=0
+    for(let str of s){
+        if(str=='|')barCount++
+        if(barCount%2==0){
+            if(str=='*')count++
+        }
+    }
+ return count   
+};
