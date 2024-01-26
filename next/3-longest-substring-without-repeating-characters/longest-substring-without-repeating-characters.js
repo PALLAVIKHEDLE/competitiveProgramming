a09757dbdbbeb612cb26ca5e4591057008a19942
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-25 18:14:22 -0800

    Time: 105 ms (32.82%) | Memory: 56.4 MB (7.07%) - LeetSync

--- next/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ next/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -5,19 +5,20 @@
 var lengthOfLongestSubstring = function(s) {
   let map={},maxString=start=count=0
        for(let i=0;i<s.length;i++){
-           console.log('index',i, start)
            if(map[s[i]]==undefined||map[s[i]]==0){
                map[s[i]]=1
                count++
                maxString=Math.max(maxString, count)
            }else{
+               // If the current character is repeated
                while (start < i && map[s[i]] !== undefined && map[s[i]] > 0) {
+               // Reset the occurrences of characters in the substring
                 map[s[start]]=0;
                 start++;
                 count--;
             }
-            map[s[i]] = 1;
-            count=i-start+1;
+            map[s[i]] = 1;// Set count to 1 for the current character
+            count=i-start+1;// Update the count based on the current substring length
            }
        }
        return maxString
