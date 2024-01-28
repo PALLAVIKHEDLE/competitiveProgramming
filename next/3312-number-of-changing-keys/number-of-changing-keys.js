cfaea17147e51a22d0dd1eca6c92f5abf3e95771
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-28 09:27:50 -0800

    Time: 60 ms (100.00%) | Memory: 51.2 MB (100.00%) - LeetSync

---
+++ next/3312-number-of-changing-keys/number-of-changing-keys.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countKeyChanges = function(s) {
+    if(!s.length) return 0
+    let count=0
+    let lastKey=s[0].toLowerCase()// Initialize with the lowercase of the first key
+
+    for(let i=0; i<s.length;i++){
+        let char=s[i].toLowerCase()
+        if(char!==lastKey){
+            count++
+            lastKey=char
+        }   
+    }
+    return count
+};   
