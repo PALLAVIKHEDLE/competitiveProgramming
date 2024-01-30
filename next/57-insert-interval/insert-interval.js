467c7f755a7789557233ae2f1476a3b40341d6c8
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-30 12:46:28 -0800

    Time: 63 ms (72.31%) | Memory: 52 MB (22.48%) - LeetSync

--- next/57-insert-interval/insert-interval.js
+++ next/57-insert-interval/insert-interval.js
@@ -4,26 +4,18 @@
  * @return {number[][]}
  */
 var insert = function(intervals, newInterval) {
-   let result =[],i;
+  let result=[], i;
 
-    for (i = 0; i < intervals.length; i++) {
-        if (intervals[i][1] < newInterval[0]) result.push(intervals[i]);
-        else if (intervals[i][0] > newInterval[1])break;
-        else {
-            newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
-            newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
-        }
-    }
+  for(i=0; i<intervals.length; i++){
+      if(intervals[i][1]<newInterval[0])result.push(intervals[i])
+      else if(intervals[i][0]>newInterval[1])break
+      else{
+          newInterval[0]=Math.min(intervals[i][0], newInterval[0])
+          newInterval[1]=Math.max(intervals[i][1], newInterval[1])
+      }
+  }
+  result.push(newInterval)
+  for(i; i<intervals.length;i++) result.push(intervals[i])
 
-    result.push(newInterval);
-
-    for (i; i < intervals.length; i++)result.push(intervals[i]);
-    
-return result;
+return result  
 };
- 
-
-
-
-
-  
