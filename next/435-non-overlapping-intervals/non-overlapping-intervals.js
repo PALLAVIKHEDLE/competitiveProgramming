1713f07cbc9bd523c1e258ac059a827c75624640
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-30 11:27:44 -0800

    Time: 192 ms (74.44%) | Memory: 80.5 MB (14.21%) - LeetSync

---
+++ next/435-non-overlapping-intervals/non-overlapping-intervals.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var eraseOverlapIntervals = function(intervals) {
+     let count=prev=0
+     intervals.sort((a,b)=>a[1]-b[1])//[[1,2],[2,3],[1,3],[3,4]]
+
+     for(let i=1; i<intervals.length;i++){
+         if(intervals[i][0]<intervals[prev][1])count++
+         else prev=i
+     }
+ return count    
+};
