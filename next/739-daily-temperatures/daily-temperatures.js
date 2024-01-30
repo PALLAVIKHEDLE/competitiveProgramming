3005d7e7971e4733ed8e241097caf94bdbe0bb60
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-29 17:21:27 -0800

    Time: 261 ms (24.41%) | Memory: 69.3 MB (51.52%) - LeetSync

--- next/739-daily-temperatures/daily-temperatures.js
+++ next/739-daily-temperatures/daily-temperatures.js
@@ -3,14 +3,14 @@
  * @return {number[]}
  */
 var dailyTemperatures = function(temperatures) {
-    if(temperatures.length==0)return []
-    let stack=[], res=new Array(temperatures.length)
-    for(let i=temperatures.length-1; i>=0;i--){
-        while(stack.length &&  temperatures[stack[stack.length-1]]<=temperatures[i]){
-            stack.pop()
-        }
-        res[i]=stack.length>0?stack[stack.length-1]-i:0
-        stack.push(i)
-    }
+//stack is used to store the indexes of the elements which are larger than the current element 
+ let stack=[], res=[];
+ for(let i=temperatures.length-1; i>=0; i--){
+     //we pop the indexes of the elements which are les than or equal to the current element
+     while(temperatures[stack[stack.length-1]]<=temperatures[i])stack.pop()
+     //if stack is empty, It means there is no larger element on the right 
+     res[i]=!stack.length?0: stack[stack.length-1]-i
+     stack.push(i)
+ }
 return res
 };
