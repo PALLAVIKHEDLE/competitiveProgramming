1b3d1d894692c9c3523de5192524a8f2d729d4e3
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-30 16:11:09 -0800

    Time: 445 ms (71.91%) | Memory: 57.2 MB (18.54%) - LeetSync

--- next/2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.js
+++ next/2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.js
@@ -2,18 +2,14 @@
  * @param {string} s
  * @return {number}
  */
-
 var countPalindromicSubsequence = function(s) {
-    let count=0,//total count of unique palindrom
-        chars=new Set(s);//unique char in s
-    console.log('set', chars)
+    let count=0,// total count of unique palindrome
+        chars=new Set(s)// unique char in s
 
-    for(let char of chars){
-  //finds the indices of the first and last occurrences of that character in the input string s.     
-        let first=s.indexOf(char),
-            last=s.lastIndexOf(char);
-        console.log('chars', first, last)
-            count+=new Set(s.slice(first+1,last)).size
-    }
-return count
+     for(let char of chars){
+         let first= s.indexOf(char),
+             last=s.lastIndexOf(char)
+             count+=new Set(s.slice(first+1,last)).size
+     }  
+ return count     
 };
