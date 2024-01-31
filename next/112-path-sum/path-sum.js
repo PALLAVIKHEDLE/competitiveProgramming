55e93f0cac811e1a838d23dfa758ab5dad770414
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-01-31 11:28:53 -0800

    Time: 48 ms (97.59%) | Memory: 51.9 MB (11.47%) - LeetSync

--- next/112-path-sum/path-sum.js
+++ next/112-path-sum/path-sum.js
@@ -12,9 +12,8 @@
  * @return {boolean}
  */
 var hasPathSum = function(root, targetSum) {
-if(!root) return false//empty tree or root null
-if(root.val==targetSum &&!root.left &&!root.right) return true //if there is only a root node 
-// Call the same function recursively for left and right subtree...
+    if(!root) return false
+    if(root.val==targetSum && !root.left && !root.right) return true
 
-return  hasPathSum(root.left, targetSum- root.val) || hasPathSum(root.right, targetSum-root.val)
+    return hasPathSum(root.left, targetSum-root.val)||hasPathSum(root.right, targetSum-root.val)
 };
