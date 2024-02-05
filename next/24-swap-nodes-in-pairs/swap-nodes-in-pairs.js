14c72ab46bb546e8234c3d994b05a6501fda7382
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-02-05 10:00:48 -0800

    Time: 47 ms (84.73%) | Memory: 48.9 MB (19.56%) - LeetSync

--- next/24-swap-nodes-in-pairs/swap-nodes-in-pairs.js
+++ next/24-swap-nodes-in-pairs/swap-nodes-in-pairs.js
@@ -10,23 +10,17 @@
  * @return {ListNode}
  */
 var swapPairs = function(head) {
-     let dummyNode = new ListNode(0, head);
-     let current = dummyNode;
+    let dummyNode=new ListNode(0, head)
+    let current=dummyNode
+    while(current.next!=null && current.next.next!=null){
+        let swap1= current.next
+        let swap2=current.next.next
 
-   // Iterate through the linked list while there are at least two more nodes
-   while ( current.next !== null && current.next.next !== null) {
-       let swap1 = current.next;      // First node to be swapped
-       let swap2 = current.next.next; // Second node to be swapped
-       
-       // Perform the swap
-       swap1.next = swap2.next; // Link the first node to the node after the second node
-       swap2.next = swap1;      // Link the second node to the first node
-
-       current.next = swap2;    // Link the current node to the second swapped node
-       current = swap1;         // Move the current pointer to the next pair
-
-       // At this point, the current pair has been swapped, and the current pointer is moved to the next pair
-   }
-
-   return dummyNode.next; 
+        swap1.next=swap2.next
+        swap2.next=swap1
+        
+        current.next=swap2
+        current=swap1
+    }
+return dummyNode.next
 };
