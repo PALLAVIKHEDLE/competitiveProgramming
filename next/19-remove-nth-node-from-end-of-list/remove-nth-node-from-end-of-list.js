b3e6720f1f3a6df4f17a90d418806e22a42e6764
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-02-05 09:28:48 -0800

    Time: 63 ms (26.31%) | Memory: 50.6 MB (13.43%) - LeetSync

--- next/19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.js
+++ next/19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.js
@@ -11,14 +11,13 @@
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
-    let fast=slow=head//Initialize fast and slow pointers to the head of the linked list
-    for(let i=0; i<n;i++)fast=fast.next//move the first pointer upto n
+    let fast=slow=head
+    for(let i=0; i<n; i++) fast=fast.next
     if(!fast)return head.next
-    // Move both pointers until fast reaches the end of the linked list
     while(fast.next){
         fast=fast.next
         slow=slow.next
     }
-    slow.next=slow.next.next//Remove the nth node from the end
-return head    
+    slow.next=slow.next.next
+return head
 };
