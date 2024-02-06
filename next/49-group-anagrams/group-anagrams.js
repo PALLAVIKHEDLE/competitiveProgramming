fa6e7688611c1089e8fc845893ebcd20fe623730
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-02-05 20:08:34 -0800

    Time: 86 ms (98.83%) | Memory: 63.7 MB (7.69%) - LeetSync

--- next/49-group-anagrams/group-anagrams.js
+++ next/49-group-anagrams/group-anagrams.js
@@ -3,11 +3,11 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-    let map={}
-    for(let ana of strs){
-        let s=ana.split('').sort().join('')
-        if(!map[s])map[s]=[]
-        map[s].push(ana)
-    }
-return Object.values(map)
+  let map={}
+  for(let str of strs){
+      let s=str.split('').sort().join('')
+      if(!map[s])map[s]=[]
+      map[s].push(str)
+  }
+  return Object.values(map)
 }
