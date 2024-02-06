38cfbeb6887945e2f9558b0b6a1e8816a3f02aaf
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-02-06 14:54:08 -0800

    Time: 65 ms (38.78%) | Memory: 52.2 MB (6.80%) - LeetSync

--- next/998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.js
+++ next/998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.js
@@ -11,18 +11,17 @@
  * @return {boolean}
  */
 var isCompleteTree = function(root) {
-    let seenNull=false
-    let queue=[root]
+    let seenNull=false, queue=[root]
     while(queue.length){
-       const result=[]
-        for(let nextNode of queue){
-            if(!nextNode) seenNull=true
-            else{
-                if(seenNull)return false
-                result.push(nextNode.left,nextNode.right)
-            }
-        }
-        queue=result
+        const result=[]
+         for(let nextNode of queue){
+             if(!nextNode) seenNull =true
+             else{
+                 if(seenNull)return false
+                 result.push(nextNode.left,nextNode.right)
+             }
+         }
+         queue=result
     }
-return true
+return true    
 };
