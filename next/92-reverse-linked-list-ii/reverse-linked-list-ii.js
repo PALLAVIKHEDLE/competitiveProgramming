c2cde16097f381397172257e4d7b94dbaf69875a
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-02-07 12:29:54 -0800

    Time: 49 ms (77.34%) | Memory: 49.2 MB (13.46%) - LeetSync

--- next/92-reverse-linked-list-ii/reverse-linked-list-ii.js
+++ next/92-reverse-linked-list-ii/reverse-linked-list-ii.js
@@ -12,21 +12,20 @@
  * @return {ListNode}
  */
 var reverseBetween = function(head, left, right) {
-   //Two pointer
-   if(!head ||left==right) return head
-  //Create a dummy node and connect its next to the head of the list.
-   let dummy=new ListNode(0)
-   dummy.next=head
-   let prev=dummy//Initialize a prev pointer to the dummy node
-   for(let i=0; i<left-1;i++){
-       prev=prev.next
-   }
-   let curr=prev.next
-   for(let i=0; i<right-left; i++){
-       const nextNode=curr.next
-       curr.next=nextNode.next
-       nextNode.next=prev.next
-       prev.next=nextNode
-   }
+    //TWO POINTERS
+    if(!head || left==right)return head
+    //Create a dummy node and connect its next to the head of the list
+    let dummy=new ListNode(0, head)
+ 
+    let prev=dummy // Initialize a prev pointer to the dummy node
+    for(let i=0; i<left-1;i++) prev=prev.next
+
+    let curr=prev.next// use a current pointer to keep track of the first node in the sublist
+    for(let i=0; i<right-left; i++){
+        const nextNode=curr.next
+        curr.next=nextNode.next
+        nextNode.next=prev.next
+        prev.next=nextNode
+    }
 return dummy.next
 };
