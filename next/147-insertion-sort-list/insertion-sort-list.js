05a50ecafde1ffb5b31e7f601dec660b35208c7e
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-02-07 11:10:19 -0800

    Time: 71 ms (78.60%) | Memory: 53.1 MB (19.93%) - LeetSync

--- next/147-insertion-sort-list/insertion-sort-list.js
+++ next/147-insertion-sort-list/insertion-sort-list.js
@@ -11,24 +11,25 @@
  */
 var insertionSortList = function(head) {
     if(!head)return null
-    let dummyNode=new ListNode(0, head)
-    let prev=head,curr=head.next
+    let dummyNode= new ListNode(0, head)
+    let prev=head, curr=head.next
     while(curr){
         if(curr.val>=prev.val){
             prev=curr
             curr=curr.next
         }else{
-            let temp=dummyNode
-            //find correct position to insert current node
-            while(temp.next && temp.next.val<curr.val){
-                temp=temp.next
-            }
-            prev.next=curr.next// remove current node
-            curr.next=temp.next// insert current node
+            let temp= dummyNode
+            //find the correct position to insert the current node
+            while(temp.next && temp.next.val<curr.val) temp=temp.next
+
+            prev.next=curr.next //remove current node
+            curr.next=temp.next//insert current node
             temp.next=curr
-            // things sorted out, let's move to the next pointer
+             // things sorted out, let's move to the next pointer
             curr=prev.next
         }
     }
-return dummyNode.next    
+    
+return dummyNode.next   
 };
+   
