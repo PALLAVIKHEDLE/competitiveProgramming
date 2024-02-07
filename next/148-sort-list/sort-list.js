293af84e92dfa1edcf960ece5a3ec2c2a737c1c3
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-02-07 12:13:51 -0800

    Time: 183 ms (19.92%) | Memory: 76 MB (16.81%) - LeetSync

--- next/148-sort-list/sort-list.js
+++ next/148-sort-list/sort-list.js
@@ -10,27 +10,29 @@
  * @return {ListNode}
  */
 var sortList = function (head) {
-    if (!head || !head.next) return head;
-    let fast = slow = head;
-    while (fast.next && fast.next.next) {
-        fast = fast.next.next;
-        slow = slow.next;
+    if(!head || !head.next) return head
+    let fast=slow=head
+    while(fast.next && fast.next.next){
+        fast=fast.next.next
+        slow=slow.next
+
     }
-    let middle = slow.next;
+    let middle=slow.next
     slow.next=null
-    return merge(sortList(head), sortList(middle));
+    return merge(sortList(head), sortList(middle))
 };
 
 const merge=(first, second)=>{
     const dummyNode= new ListNode(0)
-    let temp=dummyNode
+    let temp= dummyNode
     while(first && second){
         temp.next=(first.val<second.val)?first:second
         temp=temp.next
         if(first.val<second.val)first=first.next
         else second=second.next
     }
-    if(first)temp.next=first
-    if(second)temp.next=second
-    return dummyNode.next
+if(first)temp.next=first
+if(second)temp.next=second 
+ return dummyNode.next
 }
+
