a0830f08570f57e5fc249459de45cb94c51dc312
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-02-06 23:27:16 -0800

    Time: 61 ms (79.26%) | Memory: 54.4 MB (10.11%) - LeetSync

---
+++ next/662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var widthOfBinaryTree = function(root) {
+    //index represents the position of the node
+    let stack=[{node:root, index:1}]
+    let maxWidth=1;
+
+    while(stack.length>0){
+        maxWidth=Math.max(maxWidth, (stack[stack.length-1].index-stack[0].index)+1)
+        for(let i=stack.length-1; i>=0;i--){
+            let {node, index}=stack.shift()
+            if(node.left)stack.push({node:node.left, index:index*2})
+            if (node.right) stack.push({node: node.right, index: (index * 2) + 1})
+        }
+         if (stack.length === 1) stack[0].index = 1
+    }
+ return maxWidth
+};
