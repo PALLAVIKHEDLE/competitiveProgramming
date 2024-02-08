b87756a01f231eec0eb9667ac33f96fd10485345
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-02-08 09:07:50 -0800

    Time: 53 ms (93.15%) | Memory: 51.7 MB (16.44%) - LeetSync

---
+++ next/1222-remove-covered-intervals/remove-covered-intervals.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var removeCoveredIntervals = function(intervals) {
+    let prevEnd = -1,count = 0;
+    
+    intervals.sort((a, b) => {
+        if (a[0] !== b[0]) return a[0] - b[0];
+        else return b[1] - a[1];
+    });
+
+    for (const interval of intervals) {
+        const [start, end] = interval;
+        if (end > prevEnd) {
+            count++;
+            prevEnd = end;
+        }
+    }
+
+    return count;
+};
