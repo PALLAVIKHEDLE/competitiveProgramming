989049f1ee69e5fbe8e80d8a6d45919f868e37c9
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-02-09 14:41:50 -0800

    Time: 121 ms (29.50%) | Memory: 68.1 MB (17.99%) - LeetSync

---
+++ next/652-find-duplicate-subtrees/find-duplicate-subtrees.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode[]}
+ */
+var findDuplicateSubtrees = function(root) {
+    let map={}, result=[]
+    function traverse(node){
+        if(!node)return 'null'
+        let left=traverse(node.left)
+        let right=traverse(node.right)
+
+//It creates a unique key for the current subtree
+        let key=`${node.val}-${left}-${right}`;
+
+        map[key]=(map[key]||0)+1
+        if(map[key]==2)result.push(node)
+        return key
+    }
+    traverse(root)
+    return result
+};
