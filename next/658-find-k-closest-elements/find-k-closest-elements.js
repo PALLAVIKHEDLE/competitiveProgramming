0c78f3960f87525ed1ad756bcbe715b38a81abea
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-02-09 08:59:33 -0800

    Time: 74 ms (83.36%) | Memory: 55.6 MB (32.97%) - LeetSync

--- next/658-find-k-closest-elements/find-k-closest-elements.js
+++ next/658-find-k-closest-elements/find-k-closest-elements.js
@@ -5,12 +5,14 @@
  * @return {number[]}
  */
 var findClosestElements = function(arr, k, x) {
-    let left=0, right=arr.length-1
-    while(right-left>=k){
-        if(Math.abs(arr[left]-x)<=Math.abs(arr[right]-x))right--
-        else left++
-    }
-    return arr.slice(left, right+1)
+let start=0, end=arr.length-1
+while(end-start>=k){
+    if(Math.abs(arr[start]-x)<=Math.abs(arr[end]-x))end--
+    else start++
+}
+return arr.slice(start, end+1)
+};
+  
 //     let result=[], diffsWithIndices=[]
 //     for(let i=0; i<arr.length;i++){
 //         let diff=Math.abs(x-arr[i])
@@ -22,4 +24,3 @@ var findClosestElements = function(arr, k, x) {
 //     result.sort((a,b)=>a-b)
 
 //   return result  
-};
