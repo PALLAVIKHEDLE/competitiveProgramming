076a16f45c15dca43dfdc455c440f9061111a0ab
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-02-15 08:48:22 -0800

    Time: 67 ms (76.75%) | Memory: 50.2 MB (40.28%) - LeetSync

--- next/424-longest-repeating-character-replacement/longest-repeating-character-replacement.js
+++ next/424-longest-repeating-character-replacement/longest-repeating-character-replacement.js
@@ -4,17 +4,16 @@
  * @return {number}
  */
 var characterReplacement = function(s, k) {
-    let map={}, start=0, maxCharCount=Number.MIN_SAFE_INTEGER,maxLength=0
-    for(let i=0; i<s.length;i++){
-        if(map[s[i]]==undefined)map[s[i]]=1
-        else map[s[i]]++
-
-        maxCharCount=Math.max(maxCharCount,map[s[i]])
+  let map={}, start=0, maxCharCount=Number.MIN_SAFE_INTEGER, maxLength=0,currWindow
+  for(let i=0;i<s.length;i++){
+     map[s[i]]=(map[s[i]]||0)+1
+    
+     maxCharCount=Math.max(maxCharCount, map[s[i]]) 
         while((i-start+1)-maxCharCount>k){
             map[s[start]]--
             start++
         }
-        maxLength=Math.max(maxLength,i-start+1)
-    }
+  maxLength=Math.max(maxLength,i-start+1)
+  }
   return maxLength
 };
