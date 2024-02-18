9fb9f37619ac05d521264fa6cb116039d8bbcec2
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-02-18 09:35:24 -0800

    Time: 87 ms (65.71%) | Memory: 55.1 MB (38.62%) - LeetSync

--- next/23-merge-k-sorted-lists/merge-k-sorted-lists.js
+++ next/23-merge-k-sorted-lists/merge-k-sorted-lists.js
@@ -9,32 +9,31 @@
  * @param {ListNode[]} lists
  * @return {ListNode}
  */
- function mergeLists(l1,l2){
-     const dummy=new ListNode(0)
-     let current=dummy
-     if(!l1 || !l2) return l1?l1:l2
-
-     while(l1 && l2){
-         if(l1.val<l2.val){
-             current.next=l1
-             l1=l1.next
-         }else{
-             current.next=l2
-             l2=l2.next
-         }
-         current=current.next
-     }
-     current.next=l1||l2
-  return dummy.next   
- }
 var mergeKLists = function(lists) {
-  if(lists.length==0)return null
-  //it takes 2 list at a time, loop continues until there is only one list left
-  while(lists.length>1){
-      let l1=lists.shift()
-      let l2=lists.shift()
-      const val=mergeLists(l1,l2)
-      lists.push(val)
-  } 
-  return lists[0] 
+    if(lists.length==0)return null
+    //it takes 2 list at a time, loop continue until there is only one list left
+    while(lists.length>1){
+        let l1=lists.shift()
+        let l2=lists.shift()
+        const val=mergeLists(l1,l2)
+        lists.push(val)
+    }
+ return lists[0]   
 };
+function mergeLists(l1,l2){
+        const dummy=new ListNode(0)
+        let current=dummy
+        if(!l1 || !l2)return l1?l1:l2
+        while(l1&& l2){
+            if(l1.val<l2.val){
+                current.next=l1
+                l1=l1.next
+            }else{
+                current.next=l2
+                l2=l2.next
+            }
+            current=current.next
+        }
+        current.next=l1||l2
+        return dummy.next
+}
