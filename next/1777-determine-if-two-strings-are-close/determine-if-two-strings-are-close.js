298f56c4e7a6bcee93ea11be37c474a17ac4deae
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-02-20 09:10:48 -0800

    Time: 145 ms (50.44%) | Memory: 57.2 MB (16.92%) - LeetSync

--- next/1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.js
+++ next/1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.js
@@ -4,24 +4,21 @@
  * @return {boolean}
  */
 var closeStrings = function(word1, word2) {
-    if(word1.length!==word2.length)return false
-    let map1={}, map2={}
-    for(let word of word1){
-        if(map1[word]==undefined)map1[word]=1
-        else map1[word]++
-    }
-     for(let char of word2){
-        if(map2[char]==undefined)map2[char]=1
-        else map2[char]++
-    }
-     // Check if the sets of characters are the same
-    let chars1 = Object.keys(map1).sort();
-    let chars2 = Object.keys(map2).sort();
-     if (chars1.join('') !== chars2.join(''))return false;
- 
-    // Check if the frequencies of each character are the same
-    let frequencies1 = Object.values(map1).sort();
-    let frequencies2 = Object.values(map2).sort();
+  let map1={}, map2={}
+  if(word1.length!==word2.length)return false
+  for(let word of word1){
+      map1[word]=(map1[word]||0)+1
+  }
+  for(let char of word2){
+      map2[char]=(map2[char]||0)+1
+  }
+  //check if the sets of character are the same
+  let chars1=Object.keys(map1).sort();
+  let chars2=Object.keys(map2).sort();
+  if(chars1.join('')!==chars2.join(''))return false
 
-    return frequencies1.join('') === frequencies2.join('');
+  //check the frequencies of each character are the same
+  let freq1=Object.values(map1).sort()
+  let freq2=Object.values(map2).sort()
+return freq1.join('') === freq2.join('');
 };
