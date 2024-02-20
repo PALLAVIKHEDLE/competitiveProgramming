980c5114c1e9a5e5c036e1a8be3050038d24493b
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-02-19 17:44:06 -0800

    Time: 62 ms (52.02%) | Memory: 49.4 MB (84.85%) - LeetSync

--- next/3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.js
+++ next/3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.js
@@ -2,20 +2,18 @@
  * @param {string[]} words
  * @return {number}
  */
-function isPrefixAndSuffix(word1, word2) {
-    return word2.startsWith(word1) && word2.endsWith(word1);
-}
+
 
 var countPrefixSuffixPairs = function(words) {
-    let count = 0;
-
-    for (let i = 0; i < words.length - 1; i++) {
-        for (let j = i + 1; j < words.length; j++) {
-            if (isPrefixAndSuffix(words[i], words[j])) {
-                count++;
+  let count = 0, n = words.length
+    
+    for(let i=0;i<n;i++) {
+        for(let j = i+1; j < n; j++) {
+            if(words[j].startsWith(words[i]) && words[j].endsWith(words[i])) {
+                count++
             }
         }
     }
-
-    return count;
+    
+    return count
 };
