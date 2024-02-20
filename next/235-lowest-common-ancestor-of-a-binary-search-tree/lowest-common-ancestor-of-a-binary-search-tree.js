63ec69eea4f14477e8ea2ea6ad3c5b30c266fbbb
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-02-20 09:00:34 -0800

    Time: 80 ms (27.61%) | Memory: 59.4 MB (16.19%) - LeetSync

--- next/235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.js
+++ next/235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.js
@@ -13,8 +13,8 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
-    if(p.val<root.val && q.val<root.val)return lowestCommonAncestor(root.left, p,q)
-    if(p.val>root.val && q.val>root.val) return lowestCommonAncestor(root.right,p,q)
-    return root
+    if(p.val<root.val && q.val<root.val) return lowestCommonAncestor(root.left,p,q)
+    if(p.val>root.val && q.val >root.val) return lowestCommonAncestor(root.right,p,q)
     
+    return root
 };
