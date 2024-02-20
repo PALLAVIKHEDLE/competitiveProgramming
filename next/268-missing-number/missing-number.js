416f4378a922e02d9b21c382d8eb9fc68bee9820
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-02-19 17:30:36 -0800

    Time: 270 ms (10.34%) | Memory: 51.6 MB (20.82%) - LeetSync

--- next/268-missing-number/missing-number.js
+++ next/268-missing-number/missing-number.js
@@ -3,8 +3,9 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-    nums.sort((a,b)=>a-b)
-    for(let i=0; i<=nums.length;i++){
-        if(i!==nums[i]) return i
-    }
+let minVal=0
+for(let i=0; i<=nums.length;i++){
+    if(nums.includes(minVal))minVal++
+    else return minVal
+}
 };
