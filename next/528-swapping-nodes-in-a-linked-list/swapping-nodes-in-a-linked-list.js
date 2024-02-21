dd68830059ed695fcb31553b8aac4bf0f5cd6a8f
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-02-21 08:46:28 -0800

    Time: 369 ms (65.79%) | Memory: 82.5 MB (94.21%) - LeetSync

--- next/528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.js
+++ next/528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.js
@@ -16,7 +16,7 @@ var swapNodes = function(head, k) {
   for(let i=1; i<k; i++)fast=fast.next
   //store the node at position k in temp
   temp=fast
-  //move bothe the pointer until fast reaches the end
+  //move both the pointer until fast reaches the end
   while(fast.next){
       fast=fast.next
       slow=slow.next
