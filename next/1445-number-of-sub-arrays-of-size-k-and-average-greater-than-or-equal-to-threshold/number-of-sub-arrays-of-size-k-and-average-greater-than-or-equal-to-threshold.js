719f39f03b7dd4a82b3b924c086e31550b4f33eb
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-02-22 09:12:37 -0800

    Time: 75 ms (30.63%) | Memory: 56.1 MB (50.45%) - LeetSync

--- next/1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.js
+++ next/1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.js
@@ -5,11 +5,26 @@
  * @return {number}
  */
 var numOfSubarrays = function(arr, k, threshold) {
-    let start=0, count=0, sum=0
-    for(let i=0; i<arr.length;i++){
-        sum+=arr[i]
-        while(i-start+1>k) sum-=arr[start++]
-        if(i-start+1===k&&sum/k>=threshold)count++
-    }
-return count    
+       let start=0,end=arr.length-1,left=0, sum=0,count=0
+       while(start<=end){
+           sum+=arr[start]
+           while(start-left+1>k)sum-=arr[left++]
+           if(start-left+1==k && sum/k>=threshold)count++
+           start++
+       }
+  return count     
 };
+
+
+
+
+
+
+
+// let start=0, count=0, sum=0
+//     for(let i=0; i<arr.length;i++){
+//         sum+=arr[i]
+//         while(i-start+1>k) sum-=arr[start++]
+//         if(i-start+1===k&&sum/k>=threshold)count++
+//     }
+// return count 
