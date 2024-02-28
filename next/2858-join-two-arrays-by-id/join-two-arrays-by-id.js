7ee3a61ec4012f7df5434d41a126e7da371a4021
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-02-28 12:31:04 -0800

    Time: 314 ms (69.01%) | Memory: 90.7 MB (86.76%) - LeetSync

---
+++ next/2858-join-two-arrays-by-id/join-two-arrays-by-id.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Array} arr1
+ * @param {Array} arr2
+ * @return {Array}
+ */
+var join = function(arr1, arr2) {
+  let map={}
+  for(let i=0; i<arr1.length;i++){
+      map[arr1[i].id]=arr1[i]
+  }  
+   for(let i=0; i<arr2.length;i++){
+      if(map[arr2[i].id]){
+          for(let key in arr2[i])map[arr2[i].id][key]=arr2[i][key]
+      }else map[arr2[i].id]=arr2[i]
+  }  
+     return Object.values(map);
+};
