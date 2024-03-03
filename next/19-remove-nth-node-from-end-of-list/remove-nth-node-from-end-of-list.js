60d2fa42f3b281b7a883dae4d7fb3b1163e9d101
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-03-02 16:16:17 -0800

    Time: 51 ms (82.34%) | Memory: 50.2 MB (60.13%) - LeetSync

--- next/19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.js
+++ next/19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.js
@@ -11,14 +11,15 @@
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
-   let fast=slow=head
-   for(let i=0; i<n;i++)fast=fast.next
-   if(!fast)return head.next
-
-   while(fast && fast.next){
-       fast=fast.next
-       slow=slow.next
-   }
-   slow.next=slow.next.next
-   return head
+  let fast=slow=head
+  for(let i=0;i<n;i++){
+      fast=fast.next
+  }
+  if(!fast) return head.next
+  while(fast && fast.next){
+      fast=fast.next
+      slow=slow.next
+  }
+  slow.next=slow.next.next
+  return head 
 };
