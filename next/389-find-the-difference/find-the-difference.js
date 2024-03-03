c31a90b6970043accbbf1a8d469ec5d1055fda12
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-03-03 15:45:04 -0800

    Time: 57 ms (66.75%) | Memory: 50.7 MB (44.11%) - LeetSync

---
+++ next/389-find-the-difference/find-the-difference.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+var findTheDifference = function(s, t) {
+    let map={}
+    for(let str1 of s){
+        map[str1]=(map[str1]||0)+1
+    }
+     for(let i=0; i<t.length;i++){
+       let val=map[t[i]]
+       if(val!==undefined)map[t[i]]=val-1
+       else map[t[i]]=1
+    }
+    for(key in map){
+        if(map[key]!==0)return key
+    }
+};
