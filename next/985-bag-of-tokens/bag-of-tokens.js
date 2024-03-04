75f24ab5d00b98a8fce7de5fc3220de7447b21e4
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-03-03 17:26:04 -0800

    Time: 65 ms (57.14%) | Memory: 50.9 MB (14.29%) - LeetSync

--- next/985-bag-of-tokens/bag-of-tokens.js
+++ next/985-bag-of-tokens/bag-of-tokens.js
@@ -6,16 +6,19 @@
 var bagOfTokensScore = function(tokens, power) {
      tokens.sort((a, b) => a - b); // Sort tokens in ascending order
 
-    let score = 0, maxScore = 0;
-    let i = 0,j = tokens.length - 1;
+    let score = 0, //keeps track of the current score.
+        maxScore = 0; //keeps track of the maximum score achieved so far
+        let i=0, j=tokens.length-1
 
-    while (i <= j) {
+    while (i <=j) {
+        //  token is played face-up, power is reduced by the token value,
         if (power >= tokens[i]) {
             power -= tokens[i];
             score++;
             i++;
             maxScore = Math.max(maxScore, score);
         } else if (score > 0) {
+            // played face-down, power is increased
             power += tokens[j];
             score--;
             j--;
