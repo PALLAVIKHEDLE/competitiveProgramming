7a74a17e1efb3c0c0a41117154987b56c75b00e7
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-03-04 17:55:25 -0800

    Time: 58 ms (84.21%) | Memory: 53 MB (65.79%) - LeetSync

--- next/1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.js
+++ next/1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.js
@@ -6,7 +6,6 @@ var minimumLength = function(s) {
   let i = 0, j = s.length - 1;
     while (i < j && s[i] === s[j]) {
         let char = s[i];
-        
         // Move i to the right until it's no longer equal to char
         while (i <= j && s[i] === char)i++;
           
@@ -14,8 +13,7 @@ var minimumLength = function(s) {
         while (i <= j && s[j] === char)j--;
     }
     
-    // If the string is empty or all characters are the same, return the length of the remaining string
-    if (i > j) return j - i + 1;
+    
     
     // Otherwise, there are characters left that are not part of the prefix and suffix
     // So, return the length of those characters
