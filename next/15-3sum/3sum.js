12abfdbf6e131c2b7f185f52d4cafe6ab9a05782
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-03-05 16:02:58 -0800

    Time: 152 ms (76.14%) | Memory: 65.5 MB (39.30%) - LeetSync

--- next/15-3sum/3sum.js
+++ next/15-3sum/3sum.js
@@ -3,19 +3,19 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    let result=[];
+    let result=[]
     nums.sort((a,b)=>a-b)
     for(let i=0; i<nums.length;i++){
-        let left=i+1,right=nums.length-1
-        if(i>0&& nums[i]==nums[i-1])continue
+        let left=i+1, right=nums.length-1
+        if(i>0 && nums[i]==nums[i-1])continue
         while(left<right){
             let currentSum=nums[i]+nums[left]+nums[right]
             if(currentSum==0){
-                result.push([nums[i],nums[left],nums[right]])
+                result.push([nums[i], nums[left],nums[right]]) 
                 // Skip duplicates for the second element
-                while (left < right && nums[left] === nums[left + 1])left++;
+                while(left<right && nums[left]===nums[left+1])left++
                 // Skip duplicates for the third element
-                while (left < right && nums[right] === nums[right - 1]) right--;
+                while(left<right && nums[right]===nums[right-1])right--
                 left++
                 right--
             }else if(currentSum<0)left++
