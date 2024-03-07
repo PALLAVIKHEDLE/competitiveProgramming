f41d5caa14c431cea8118917222bc386ccb569df
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-03-07 12:17:07 -0800

    Time: 63 ms (54.53%) | Memory: 52.6 MB (39.08%) - LeetSync

--- next/150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
+++ next/150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
@@ -3,20 +3,20 @@
  * @return {number}
  */
 var evalRPN = function(tokens) {
-   let stack=[], nums2, nums1
+   let stack=[], nums1, nums2
 
-    let operators={
-        '+':(nums1,nums2)=>nums1+nums2,
-        '-':(nums1,nums2)=>nums1-nums2,
-        '*':(nums1,nums2)=>nums1*nums2,
-        '/':(nums1,nums2)=>Math.trunc(nums1/nums2)
-    }
-   for(let i=0; i<tokens.length;i++){
+   let operators={
+       '+':(nums1,nums2)=>nums1+nums2,
+       '-':(nums1,nums2)=>nums1-nums2,
+       '*':(nums1,nums2)=>nums1*nums2,
+       '/':(nums1,nums2)=>Math.trunc(nums1/nums2)
+   }
+   for(let i=0;i<tokens.length;i++){
        if(operators[tokens[i]]){
            nums2=stack.pop()
            nums1=stack.pop()
            stack.push(operators[tokens[i]](nums1,nums2))
        }else stack.push(parseInt(tokens[i]))
-   } 
- return stack.pop() 
+   }
+return stack.pop()
 }
