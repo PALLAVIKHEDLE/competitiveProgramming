9b8a483445555be73af28074d838e933b2413fcb
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-03-07 12:45:31 -0800

    Time: 75 ms (74.15%) | Memory: 65.3 MB (77.56%) - LeetSync

--- next/456-132-pattern/132-pattern.js
+++ next/456-132-pattern/132-pattern.js
@@ -3,11 +3,20 @@
  * @return {boolean}
  */
 var find132pattern = function(nums) {
- let max=Number.MIN_SAFE_INTEGER,stack=[]
- for(let i=nums.length-1; i>=0;i--){
-     while(nums[i]>stack[stack.length-1])max=stack.pop()
-     if(nums[i]<max) return true
-     stack.push(nums[i])
- }
- return false
+//  let max=Number.MIN_SAFE_INTEGER,stack=[]
+//  for(let i=nums.length-1; i>=0;i--){
+//      while(nums[i]>stack[stack.length-1])max=stack.pop()
+//      if(nums[i]<max) return true
+//      stack.push(nums[i])
+//  }
+//  return false
+let stack=[], minCurr=nums[0]
+// if(length<3)return false
+for(let i=1; i<nums.length;i++){
+    while(stack.length && nums[i]>=stack[stack.length-1][0])stack.pop()
+    if(stack.length && nums[i] > stack[stack.length-1][1] )  return true;
+    stack.push([nums[i],minCurr]);
+    minCurr=Math.min(minCurr,nums[i])
+}
+return false
 };
