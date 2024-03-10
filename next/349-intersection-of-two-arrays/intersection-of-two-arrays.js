9e5bbb06a905bf36a604f97c597d983502c5769b
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-03-09 16:18:21 -0800

    Time: 49 ms (86.90%) | Memory: 49.1 MB (73.41%) - LeetSync

--- next/349-intersection-of-two-arrays/intersection-of-two-arrays.js
+++ next/349-intersection-of-two-arrays/intersection-of-two-arrays.js
@@ -4,11 +4,16 @@
  * @return {number[]}
  */
 var intersection = function(nums1, nums2) {
-    let map={}, result=[]
-    for(let num of nums1)map[num]=(map[num]=0)|1
-    for(let number of nums2){
-        if(map[number]!==undefined)if(!result.includes(number))result.push(number)    
-    }
+    // let map={}, result=[]
+    // for(let num of nums1)map[num]=(map[num]=0)|1
+    // for(let number of nums2){
+    //     if(map[number]!==undefined)if(!result.includes(number))result.push(number)    
+    // }
 
-    return result
+    // return result
+    var value = nums1.filter(x => nums2.includes(x)) // returns intersection of two arrays
+    
+var remDuplicate  = [...new Set(value)]; //removes duplicates from array
+
+return remDuplicate ;
 };
