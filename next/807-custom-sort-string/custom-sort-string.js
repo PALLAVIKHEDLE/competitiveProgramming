c3a8621c7b240f229ee615cfc1e8f3e4515efbb7
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-03-10 18:29:02 -0700

    Time: 46 ms (92.62%) | Memory: 50.1 MB (36.67%) - LeetSync

--- next/807-custom-sort-string/custom-sort-string.js
+++ next/807-custom-sort-string/custom-sort-string.js
@@ -4,18 +4,41 @@
  * @return {string}
  */
 var customSortString = function(order, s) {
-  const map = {};
-  let res=''
-    for (const char of order)  map[char] = 0;
+//   const map = {};
+//   let res=''
+//     for (const char of order)  map[char] = 0;
 
-    for (const char of s) {
-        if (map[char] !== undefined) map[char]++;
+//     for (const char of s) {
+//         if (map[char] !== undefined) map[char]++;
+//     }
+
+//     for (const char of order) res += char.repeat(map[char]);
+
+//     for (const char of s) {
+//         if (!order.includes(char))res += char;
+//     }
+//     return res;
+let map = {};
+    let res = '';
+
+    // Count frequency of characters in s
+    for (let char of s) {
+        map[char] = (map[char] || 0) + 1;
     }
 
-    for (const char of order) res += char.repeat(map[char]);
-
-    for (const char of s) {
-        if (!order.includes(char))res += char;
+    // Iterate through order, append characters in the order they appear
+    for (let char of order) {
+        if (map[char] !== undefined) {
+            res += char.repeat(map[char]);
+            delete map[char];
+        }
     }
+
+    // Append remaining characters in s that are not in order
+    for (let char in map) {
+        res += char.repeat(map[char]);
+    }
+
     return res;
+
 };
