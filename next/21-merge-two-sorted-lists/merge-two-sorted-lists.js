6b8edb9e076ac6a37345b27f56d85dde99a7058c
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-03-13 12:45:42 -0700

    Time: 70 ms (18.10%) | Memory: 51.3 MB (70.96%) - LeetSync

--- next/21-merge-two-sorted-lists/merge-two-sorted-lists.js
+++ next/21-merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -11,18 +11,7 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-    //recusive Approach
-    // if(!list1||!list2) return list1?list1:list2
-    // if(list1.val<=list2.val){
-    //     list1.next=mergeTwoLists(list1.next,list2)
-    //     return list1
-    // }else{
-    //     list2.next=mergeTwoLists(list1, list2.next)
-    //     return list2
-    // }
-    
-//second approach
-let head=new ListNode(0, null)
+   let head=new ListNode(0, null)
 let current=head//cureent use to be traversing, head will be first node
 if(!list1||!list2) return list1?list1:list2
     while(list1 && list2){
