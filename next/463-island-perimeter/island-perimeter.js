e5660903c7e18b98062f3a344e2f60d6e53e0bd9
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-03-15 12:33:39 -0700

    Time: 122 ms (80.52%) | Memory: 58.1 MB (47.60%) - LeetSync

--- next/463-island-perimeter/island-perimeter.js
+++ next/463-island-perimeter/island-perimeter.js
@@ -11,10 +11,10 @@ var islandPerimeter = function(grid) {
             if (grid[i][j] == 1) {
                 // If the current cell is land, check its neighbors
                 perimeter += 4;
-                if (i > 0 && grid[i - 1][j] == 1) perimeter--;
-                if (i < rows - 1 && grid[i + 1][j] == 1) perimeter--;
-                if (j > 0 && grid[i][j - 1] == 1) perimeter--;
-                if (j < cols - 1 && grid[i][j + 1] == 1) perimeter--;
+             
+                if(i>0 && grid[i-1][j]==1)perimeter-=2
+                if(j>0 && grid[i][j-1]==1)perimeter-=2
+
             }
         }
     }
