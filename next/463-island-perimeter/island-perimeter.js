b2b8fcf6d8bad631903774561c2dad4f85a27d10
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-03-15 12:37:58 -0700

    Time: 130 ms (61.88%) | Memory: 57.4 MB (77.49%) - LeetSync

--- next/463-island-perimeter/island-perimeter.js
+++ next/463-island-perimeter/island-perimeter.js
@@ -11,10 +11,10 @@ var islandPerimeter = function(grid) {
             if (grid[i][j] == 1) {
                 // If the current cell is land, check its neighbors
                 perimeter += 4;
-             
+//If the top neighbor is also land, it means there's a shared edge between the current cell and its top neighbor. Hence, the perimeter is reduced by 2.
+//Similarly, if the left neighbor is land, the perimeter is again reduced by 2.
                 if(i>0 && grid[i-1][j]==1)perimeter-=2
                 if(j>0 && grid[i][j-1]==1)perimeter-=2
-
             }
         }
     }
