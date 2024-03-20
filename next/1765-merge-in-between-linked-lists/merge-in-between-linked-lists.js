9ac161e6fef308a13b2526e9067a4327b5869bd2
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-03-19 19:56:05 -0700

    Time: 153 ms (53.66%) | Memory: 62.3 MB (39.02%) - LeetSync

---
+++ next/1765-merge-in-between-linked-lists/merge-in-between-linked-lists.js
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {number} a
+ * @param {number} b
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeInBetween = function(list1, a, b, list2) {
+
+    let current = list1;
+    let count = 0;
+
+    // Traverse until the node before index 'a'
+    while (count < a - 1) {
+        current = current.next;
+        count++;
+    }
+
+    let nodeBeforeA = current;
+
+    // Traverse until index 'b'
+    while (count < b) {
+        current = current.next;
+        count++;
+    }
+
+    let nodeAfterB = current.next;
+    
+    // Connect the last node of list2 to the node after 'b'
+    let tailOfList2 = list2;
+    while (tailOfList2.next !== null) {
+        tailOfList2 = tailOfList2.next;
+    }
+    tailOfList2.next = nodeAfterB;
+
+    // Connect node before 'a' to the head of list2
+    nodeBeforeA.next = list2;
+
+    return list1;
+};
+    
