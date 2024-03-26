75474b4905364511fcdfc6cf9c61ab6f90f2ce69
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-03-26 15:34:31 -0700

    Time: 285 ms (15.62%) | Memory: 79 MB (41.43%) - LeetSync

--- next/2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.js
+++ next/2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.js
@@ -5,30 +5,29 @@
  * @return {number[]}
  */
 var successfulPairs = function(spells, potions, success) {
-// let product=1, i=0, count=0, finalResult=[]
-// while(i<spells.length){
-//     for(let j=0; j<potions.length; j++){
-//     product=spells[i]*potions[j]
-//     if(product>=success) count++
+//     let finalResult=[], count=0, product=1, i=0;
+//     while(i<spells.length){
+//         for(let j=0; j<potions.length;j++){
+//             product=spells[i]*potions[j]
+//             if(product>=success)count++
+//         }
+//         finalResult.push(count)
+//         i++
+//         count=0
+
 //     }
-//     finalResult.push(count)
-//     i++
-//     count=0
-// }
-// return finalResult
+//  return finalResult   
 
-
- potions.sort((a,b)=>a-b)
-     let res=[];
-    for(s of spells){
-     let left=0, right=potions.length-1;
-        while(left<=right){
-            let mid=Math.floor((left+right)/2)
-            if(s*potions[mid]>=success)right=mid-1,index=mid;         
-            else left=mid+1
-        }
-        res.push(potions.length-left)
-    }   
+potions.sort((a,b)=>a-b)
+let res=[]
+for(s of spells){
+    let left=0, right=potions.length-1
+    while(left<=right){
+        let mid=Math.floor((left+right)/2)
+        if(s*potions[mid]>=success)right=mid-1, index=mid
+        else left=mid+1
+    }
+    res.push(potions.length-left)
+}
 return res
-
 };
