5921954c5869671712aabbcd77e60993225e9203
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-03-27 12:03:49 -0700

    Time: 76 ms (33.15%) | Memory: 54.8 MB (32.08%) - LeetSync

--- next/713-subarray-product-less-than-k/subarray-product-less-than-k.js
+++ next/713-subarray-product-less-than-k/subarray-product-less-than-k.js
@@ -6,10 +6,7 @@
 var numSubarrayProductLessThanK = function(nums, k) {
   if (k <= 1) return 0; // Handle edge case when k is less than or equal to 1
     
-    let product = 1,
-        start = 0,
-        end = 0,
-        count = 0;
+    let product = 1,start = end = 0, count = 0;
     
     while (end < nums.length) {
         product *= nums[end];
