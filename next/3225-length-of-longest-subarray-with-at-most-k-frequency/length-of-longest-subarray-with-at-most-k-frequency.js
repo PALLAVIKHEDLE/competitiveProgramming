7647e35ec82ef3539f5a748581f69fabb3d2d2fd
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-03-28 10:36:45 -0700

    Time: 142 ms (92.92%) | Memory: 64.1 MB (84.07%) - LeetSync

--- next/3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.js
+++ next/3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.js
@@ -11,27 +11,11 @@ var maxSubarrayLength = function(nums, k) {
         while (map[nums[i]] > k) {
             // now our target is to reduce the frequency of the current element by
             // removing the element from the left side of the array
-            map[nums[start++]] -= 1;
+            // map[nums[start++]] -= 1;
+            map[nums[start]]--
+            start++
         }
         maxLength = Math.max(maxLength, i - start + 1);
     }
     return maxLength;
-
-
-    // let map = {}, maxLength = 0, start = 0;
-
-    // for (let i = 0; i < nums.length; i++) {
-    //     map[nums[i]] = (map[nums[i]] || 0) + 1;
-
-    //     // Shrink the window until the condition is met
-    //     while (Math.max(...Object.values(map)) > k) {
-    //         map[nums[start]]--;
-    //         start++;
-    //     }
-
-    //     // Update the maxLength with the current window size
-    //     maxLength = Math.max(maxLength, i - start + 1);
-    // }
-
-    // return maxLength;
 };
