37098163bead1b845f655112bdd6218ba2b5f564
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-03-30 18:37:13 -0700

    Time: 68 ms (80.00%) | Memory: 59.8 MB (60.00%) - LeetSync

--- next/2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.js
+++ next/2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.js
@@ -9,10 +9,14 @@ var countSubarrays = function(nums, minK, maxK) {
     let badIdx = -1, leftIdx = -1, rightIdx = -1;
 
     for (let i = 0; i < nums.length; ++i) {
+        // if it falls outside the range [minK, maxK] update badIdx to the index i
         if (!(minK <= nums[i] && nums[i] <= maxK)) badIdx = i;
+        //If the current element is equal to minK, update leftIdx to the current index i.
         if (nums[i] === minK) leftIdx = i;
+        //If the current element is equal to maxK, update rightIdx to the current index i.
         if (nums[i] === maxK) rightIdx = i;
         
+    //This ensures that the subarray contains at least one element from each of minK and maxK, and excludes any elements outside the range.
     res += Math.max(0, Math.min(leftIdx, rightIdx) - badIdx);
     }
 
