a196ab67549977ca3518a50aa7b6122029575a3d
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-04-06 18:23:20 -0700

    Time: 53 ms (51.99%) | Memory: 48.5 MB (84.48%) - LeetSync

--- next/678-valid-parenthesis-string/valid-parenthesis-string.js
+++ next/678-valid-parenthesis-string/valid-parenthesis-string.js
@@ -5,16 +5,16 @@
 var checkValidString = function(s) {
     let validChar=[], starCheck=[]
     for(let i=0; i<s.length;i++){
-        //Identify the star & valid parenthesis position
+        //identify star and valid parenthesis position
         if(s[i]=='(')validChar.push(i)
         else if(s[i]=='*')starCheck.push(i)
-        else{
-            if(validChar.length>0) validChar.pop()//Pop from 'validChar' if '(' is available
+        else {
+            if(validChar.length>0)validChar.pop()//Pop from 'validChar' if '(' is available
             else if(starCheck.length>0) starCheck.pop()//Pop from 'starCheck' if '*' is available
             else return false
         }
     }
-// Matching Parentheses with Wildcards
+    // Matching Parentheses with Wildcards
     let i = validChar.length - 1, j = starCheck.length - 1;
     while(validChar[i]<starCheck[j]){
         validChar.pop();
@@ -24,4 +24,4 @@ var checkValidString = function(s) {
     }
      if(validChar.length === 0) return true;
      else return false;
-};     
+};
