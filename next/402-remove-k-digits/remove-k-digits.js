3befac35c0fae40d3628761f6e7154b117c008be
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-04-10 17:26:26 -0700

    Time: 63 ms (74.26%) | Memory: 54.8 MB (80.53%) - LeetSync

--- next/402-remove-k-digits/remove-k-digits.js
+++ next/402-remove-k-digits/remove-k-digits.js
@@ -6,21 +6,25 @@
 var removeKdigits = function(num, k) {
     let stack=[], removed=0
     for(const ele of num){
-        //will have to keep track of peak (previosly occured history)
-        //whenever we find peak (next element is greater than previous one) & we have k value greater than 0. will have to pop this element nd keep poping as long as the top of the stack is greater than current element.
-        while(stack.length && ele<stack[stack.length-1]&&removed<k){
-            stack.pop()
-            removed++
-        }
-        stack.push(ele)
+        //will have to keep tract pf peak(Previously occured history)
+        /*whenever we find peak(next element is greater than previous one)
+         & we have k value greater than 0. 
+         will have to pop this element and keep popping as long as the top of the stack is greater than current element */
+         while(stack.length&& ele<stack[stack.length-1] && removed<k){
+             stack.pop()
+             removed++
+         }
+         stack.push(ele)
     }
-    //remove all beginning zeros
-    while(removed < k) {
+ // Handle case when remaining k digits need to be removed
+     while(removed < k) {
         stack.pop();
         removed++;
     }  
-    // remove all beginning zeroes
-    while(stack.length && stack[0]=='0')stack.shift()
+
+    //remove starting zeros
+    while(stack.length&& stack[0]=='0')stack.shift()
 
     return stack.length?stack.join(''):'0'
+        
 };
