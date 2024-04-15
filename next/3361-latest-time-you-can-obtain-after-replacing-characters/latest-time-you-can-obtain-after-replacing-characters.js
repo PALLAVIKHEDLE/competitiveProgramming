a5e12097ca4e570c8f01e27ddd1eef9918e45ac2
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-04-14 19:14:44 -0700

    Time: 66 ms (100.00%) | Memory: 51.2 MB (100.00%) - LeetSync

---
+++ next/3361-latest-time-you-can-obtain-after-replacing-characters/latest-time-you-can-obtain-after-replacing-characters.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var findLatestTime = function(s) {
+ // Convert string to array for easier manipulation
+    s = s.split('');
+
+    if (s[0] === "?") s[0] = s[1] <= "1" || s[1] === "?" ? "1" : "0";
+    if (s[1] === "?") s[1] = s[0] === "0" ? "9" : "1";
+    if (s[3] === "?") s[3] = "5";
+    if (s[4] === "?") s[4] = "9";
+
+    // Format the resulting array back to string in "HH:MM" format
+    return s.join("");
+}  
