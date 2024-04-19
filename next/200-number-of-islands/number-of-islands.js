0bb8babfaeaa68cc76a40ff636df553e42e4513b
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-04-19 00:09:25 -0700

    Time: 66 ms (78.65%) | Memory: 52.5 MB (64.39%) - LeetSync

--- next/200-number-of-islands/number-of-islands.js
+++ next/200-number-of-islands/number-of-islands.js
@@ -3,23 +3,24 @@
  * @return {number}
  */
 var numIslands = function(grid) {
-    let totalIslands=0
-    for(let i=0; i<grid.length;i++){
-        for(let j=0; j<grid[0].length;j++){
-            if(grid[i][j]=='1')totalIslands+=island(grid, i, j)
+     let totalIslands=0
+     for(let i=0;i<grid.length;i++){
+        for(let j=0;j<grid[0].length;j++){
+            if(grid[i][j]=='1')totalIslands+=islands(grid,i,j)
         }
-    }
-return totalIslands    
+     }
+  return totalIslands   
 };
 
-function island(grid, i, j){
-    if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0')
-        return
-//adjacent positions in four directions: right, down, left, and up.
-    grid[i][j] = 0
-    island(grid, i+1, j)
-    island(grid, i, j+1)
-    island(grid, i-1, j)
-    island(grid, i, j-1)
-    return true
+//explore and mark all connected '1's 
+function islands(grid, i,j){
+    if(i<0||j<0||i>=grid.length||j>=grid[0].length||grid[i][j]=='0')return 
+    //adjacent positions in four directions: right, down, left, and up.
+    grid[i][j]=0  // Mark current cell as visited by setting it to '0'
+    islands(grid, i+1,j)
+    islands(grid, i,j+1)
+    islands(grid, i-1,j)
+    islands(grid, i,j-1)
+
+    return true// Indicates that this cell is part of an island
 }
