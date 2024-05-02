dc883682d252daf2f15f1d35bbb70aa1c4b6e085
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-05-01 21:48:29 -0700

    Time: 65 ms (74.35%) | Memory: 51.7 MB (48.70%) - LeetSync

--- next/2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.js
+++ next/2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.js
@@ -4,9 +4,8 @@
  */
 var findMaxK = function(nums) {
      let sorted = nums.sort((a, b) => b - a);
-
     for (let i = 0; i < sorted.length; i++) {
         if (sorted.includes(-sorted[i])) return sorted[i]; 
     }
-    return -1;
+    return -1;   
 };
