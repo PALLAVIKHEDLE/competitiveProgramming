c871cb362fd46117bca069715e82334ef3a45faa
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-05-02 13:45:18 -0700

    Time: 271 ms (5.03%) | Memory: 59.1 MB (5.11%) - LeetSync

---
+++ next/283-move-zeroes/move-zeroes.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+   let start=0; i=1;
+   while(i<nums.length){
+    console.log('index', start,i)
+      if(nums[start]==0 &&nums[i]!==0){
+          nums[start]=nums[i];
+          nums[i]=0
+          start++
+
+      }
+      if(nums[start]!==0)start++
+      i++
+   }
+};
