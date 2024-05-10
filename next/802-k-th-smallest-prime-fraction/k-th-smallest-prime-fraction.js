b7b7781e3b33ff91813e59c51bbaa402ea4bad07
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-05-09 22:09:46 -0700

    Time: 2734 ms (5.88%) | Memory: 177.4 MB (5.88%) - LeetSync

--- next/802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.js
+++ next/802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.js
@@ -4,39 +4,11 @@
  * @return {number[]}
  */
 var kthSmallestPrimeFraction = function(arr, k) {
-   let left = 0, right = 1;
-    let res = [];
-
-    while (left <= right) {
-        let mid = left + (right - left) / 2;
-        let j = 1, total = 0, num = 0, den = 0;
-        let maxFrac = 0;
-
-        for (let i = 0; i < arr.length; ++i) {
-            while (j < arr.length && arr[i] > arr[j] * mid) {
-                ++j;
-            }
-                
-            total += arr.length - j;
-
-            if (j < arr.length && maxFrac < arr[i] * 1.0 / arr[j]) {
-                maxFrac = arr[i] * 1.0 / arr[j];
-                num = i;
-                den = j;
-            }
-        }
-
-        if (total === k) {
-            res = [arr[num], arr[den]];
-            break;
-        }
-
-        if (total > k) {
-            right = mid;
-        } else {
-            left = mid;
+   let res=[]
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i+1; j < arr.length; j++) {
+            res.push([arr[i]/arr[j],[arr[i],arr[j]]])
         }
     }
-
-    return res;
+    return res.sort((a,b)=>a[0]-b[0])[k-1][1]
 };
