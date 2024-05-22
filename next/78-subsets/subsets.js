da1e94a65deff07c58ad0c4ce74f7aca77dafb82
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-05-22 10:13:28 -0700

    Time: 48 ms (97.96%) | Memory: 52.3 MB (57.42%) - LeetSync

---
+++ next/78-subsets/subsets.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+       let res = [[]];
+    for (let num of nums) {
+        let n = res.length;
+        for (let i = 0; i < n; ++i) {
+            let curr = res[i].slice();
+            curr.push(num);
+            res.push(curr);
+        }
+    }
+    return res;
+};
