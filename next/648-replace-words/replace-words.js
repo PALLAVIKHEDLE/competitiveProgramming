028c8fcca3313d41297647f882c9b8b1c70248f5
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-06-06 18:05:17 -0700

    Time: 463 ms (18.60%) | Memory: 57.7 MB (90.50%) - LeetSync

---
+++ next/648-replace-words/replace-words.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string[]} dictionary
+ * @param {string} sentence
+ * @return {string}
+ */
+var replaceWords = function(dictionary, sentence) {
+    const arr = sentence.split(' ');
+    for (let i = 0; i < arr.length; i++) {
+        for (let word of dictionary) {
+            if (arr[i].startsWith(word)) arr[i] = word;
+        }
+    }
+    return arr.join(' '); 
+};
