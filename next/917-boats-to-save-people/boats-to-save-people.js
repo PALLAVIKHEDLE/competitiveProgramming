65facdeabf8bbed1cb376684532d0bbb343507dc
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-06-09 13:21:29 -0700

    Time: 127 ms (68.54%) | Memory: 57.2 MB (54.53%) - LeetSync

--- next/917-boats-to-save-people/boats-to-save-people.js
+++ next/917-boats-to-save-people/boats-to-save-people.js
@@ -4,15 +4,13 @@
  * @return {number}
  */
 var numRescueBoats = function(people, limit) {
-    let minBoatCount=0, left=0;
-    let sortedArray=people.sort((a,b)=>a-b)
-    let right=sortedArray.length-1
-   while (left <= right) {
-        if (sortedArray[left] + sortedArray[right] <= limit) {
-            left++;  
-            right--; 
-        } else right--;  
-        minBoatCount++;
-    }
-    return minBoatCount;
+  people.sort((a,b)=>a-b);
+  let count=0, start=0, end=people.length-1;
+  while(start<=end){
+    let diff=limit-people[end]
+        count++
+        end--
+        if(diff>=people[start])start++
+  }
+  return count
 };
