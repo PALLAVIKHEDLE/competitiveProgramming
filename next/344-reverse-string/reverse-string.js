429f2a11241117eecfa46680eb70a7a2653ca1e8
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-06-09 13:05:06 -0700

    Time: 76 ms (67.33%) | Memory: 57.6 MB (18.43%) - LeetSync

--- next/344-reverse-string/reverse-string.js
+++ next/344-reverse-string/reverse-string.js
@@ -3,12 +3,11 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-     let start=0,end=s.length-1
-   
-    while(start<=end){
-        [s[start],s[end]]=[s[end],s[start]]
+     let start=0, end=s.length-1
+     while(start<=end){
+        [s[start], s[end]]=[s[end], s[start]]
         start++
         end--
-    }
+     }
     return s
 };
