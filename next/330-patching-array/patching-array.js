afe646036c0ea1258ddb254f9a624029971a6e5a
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-06-15 20:23:48 -0700

    Time: 50 ms (80.77%) | Memory: 48.2 MB (100.00%) - LeetSync

--- next/330-patching-array/patching-array.js
+++ next/330-patching-array/patching-array.js
@@ -4,9 +4,7 @@
  * @return {number}
  */
 var minPatches = function(nums, n) {
-      let miss = 1; // JavaScript uses let for block scope variables
-    let result = 0;
-    let i = 0;
+      let miss = 1,result = 0, i = 0;
 
     while (miss <= n) {
         if (i < nums.length && nums[i] <= miss) {
