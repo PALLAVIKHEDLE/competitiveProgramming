823735ad478d5a6585bc7e2a4e56c3318efb4514
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-06-16 09:48:34 -0700

    Time: 76 ms (49.10%) | Memory: 58.9 MB (55.88%) - LeetSync

--- next/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ next/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -3,11 +3,11 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-let maxProfit=Number.MIN_SAFE_INTEGER, sellPrice=prices[0];
-for(let i=1; i<prices.length;i++){
-    if(sellPrice>prices[i])sellPrice=prices[i]
-    let profit=Math.abs(sellPrice-prices[i])
-    maxProfit=Math.max(profit,maxProfit)
-}
-return maxProfit==Number.MIN_SAFE_INTEGER?0:maxProfit
+    let maxProfit=Number.MIN_SAFE_INTEGER, sellPrice=prices[0];
+    for(let i=1; i<prices.length;i++){
+        if(prices[i]<sellPrice)sellPrice=prices[i]
+        let profit = Math.abs(sellPrice-prices[i])
+        maxProfit=Math.max(maxProfit,profit)
+    }
+    return maxProfit==Number.MIN_SAFE_INTEGER?0:maxProfit
 }
