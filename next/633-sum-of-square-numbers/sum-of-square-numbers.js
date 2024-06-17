3dcc442ddc142eb047214018b4c2dd141feeac6b
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-06-17 08:54:27 -0700

    Time: 51 ms (91.10%) | Memory: 51.1 MB (30.82%) - LeetSync

--- next/633-sum-of-square-numbers/sum-of-square-numbers.js
+++ next/633-sum-of-square-numbers/sum-of-square-numbers.js
@@ -3,9 +3,9 @@
  * @return {boolean}
  */
 var judgeSquareSum = function(c) {
-    let end=Math.floor(Math.sqrt(c)), start=0
+    let end=Math.floor(Math.sqrt(c)), start=0;
     while(start<=end){
-        let sum=start**2+end**2
+        let sum=start**2+end**2 // start & end  raised to the power of 2
         if(sum==c)return true
         else if(sum<c)start++
         else end--
