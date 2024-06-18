37cbac4a5fa5735aef19b36ff0ce633dd945f195
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-06-18 10:00:27 -0700

    Time: 99 ms (5.25%) | Memory: 51.8 MB (28.38%) - LeetSync

--- next/21-merge-two-sorted-lists/merge-two-sorted-lists.js
+++ next/21-merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -11,23 +11,20 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-   let head=new ListNode(0, null)
-let current=head//cureent use to be traversing, head will be first node
-if(!list1||!list2) return list1?list1:list2
-    while(list1 && list2){
-        if(list1.val<=list2.val){
-            current.next=list1
-            list1=list1.next
-        }else{
-            current.next=list2
-            list2=list2.next
-        } 
-
-       current=current.next 
+  let head=new ListNode(0, null);
+  let curr=head; //cureent use to be traversing, head will be first node
+  if(!list1 || !list2)return list1?list1:list2;
+  while(list1&& list2){
+    if(list1.val<list2.val){
+        curr.next=list1
+        list1=list1.next
+    }else{
+        curr.next=list2
+        list2=list2.next
     }
-   current.next=list1||list2//After the loop, if there are remaining nodes in either list1 or list2, they are attached to the merged list.
-
-      // return next because head was initialized to null
+    curr=curr.next
+  }
+  curr.next=list1||list2; //After the loop, if there are remaining nodes in either list1 or list2, they are attached to the merged list.
+// return next because head was initialized to null
  return head.next
-
 };
