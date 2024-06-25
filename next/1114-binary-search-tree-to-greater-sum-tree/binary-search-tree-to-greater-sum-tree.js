aa96324eafacbe0fa3a91e59e29a7de5ee693e2e
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-06-25 00:31:48 -0700

    Time: 51 ms (71.33%) | Memory: 49.3 MB (53.92%) - LeetSync

---
+++ next/1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var bstToGst = function(root) {
+    let sum=0;
+    const traverse=(node)=>{
+        if(node){
+            traverse(node.right)//Traverse the right subtree
+            sum+=node.val//update the sum
+            node.val=sum // update the current node's value
+            traverse(node.left)//Traverse the left subtree
+        }
+    }
+    traverse(root)
+    return root
+};
