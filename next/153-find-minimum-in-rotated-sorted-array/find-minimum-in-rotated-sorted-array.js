7cf82f5c923567cbccf9c42b04c569303bc128fa
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-07-06 02:06:00 -0700

    Time: 67 ms (5.28%) | Memory: 48.8 MB (69.31%) - LeetSync

--- next/153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
+++ next/153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
@@ -3,24 +3,21 @@
  * @return {number}
  */
 var findMin = function(nums) {
-    // Check if the array length is 1, return the only element
-    if(nums.length==1)return nums[0]
-    // Check if the array length is 2, return the minimum of the two elements
-    if(nums.length==2)return Math.min(nums[0],nums[1])
-
-    let left=0, right=nums.length-1
-    // If the first element is less than the last element, the array is already sorted in ascending order, return the first element
-    if(nums[left]<nums[right])return nums[left]
-    while(left<=right){
+     //check if the array length is 1, return the only element
+     if(nums.length==1)return nums[0]
+      //check if the array length is 2, return the minimum of thr two elements
+     if(nums.length==1)return Math.min(nums[0], nums[1]) 
+     let left=0, right=nums.length-1
+     // if the first element is less than the last element, the array is already sorted in ascending order, return the first element
+     if(nums[left]<nums[right])return nums[left]
+     while(left<=right){
         let mid=Math.floor((left+right)/2)
         if(nums[mid]>nums[mid+1])return nums[mid+1]
         if(nums[mid-1]>nums[mid])return nums[mid]
-
-      // If the left side of the array is sorted, move the `left` pointer to the right side
+        // If the left side of the array is sorted, move the `left` pointer to the right side
         if(nums[left]<nums[mid])left=mid+1
         // Otherwise, move the `right` pointer to the left side
         else right=mid-1
-
-    }
- return 0   
+     }
+     return 0
 };
