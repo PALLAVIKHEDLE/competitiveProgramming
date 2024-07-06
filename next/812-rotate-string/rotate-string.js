8523ab455f1a2b3317e9c7823fb13f7b11cea783
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-07-06 15:47:54 -0700

    Time: 150 ms (5.18%) | Memory: 58.1 MB (5.05%) - LeetSync

--- next/812-rotate-string/rotate-string.js
+++ next/812-rotate-string/rotate-string.js
@@ -7,7 +7,7 @@ var rotateString = function(s, goal) {
     if(s.length!==goal.length)return false
     let sArr=s.split('')
     for(let i=0;i<sArr.length; i++){
-           // Rotate sArr
+        // Rotate sArr
         let rotated =sArr.slice(i).concat(sArr.slice(0,i));
         console.log(rotated)
         if(rotated.join('')===goal)return true
