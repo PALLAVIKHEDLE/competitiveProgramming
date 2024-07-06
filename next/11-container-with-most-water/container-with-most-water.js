321023de4a4846f96910512d1c7a90d66272b882
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-07-06 13:06:42 -0700

    Time: 64 ms (73.72%) | Memory: 56.9 MB (67.06%) - LeetSync

--- next/11-container-with-most-water/container-with-most-water.js
+++ next/11-container-with-most-water/container-with-most-water.js
@@ -3,12 +3,14 @@
  * @return {number}
  */
 var maxArea = function(height) {
-    let left=area=0, right=height.length-1;
+    let area=0, left=0, right= height.length-1
     while(left<right){
-        let temp=(right-left)* Math.min(height[left], height[right])
+    // area distance between 2 walls(width) * height (why min height -> water cannot go above the shorter wall.
+    //If one wall is shorter, the water level is limited by that wall.)
+        let temp= Math.min(height[left], height[right])* (right-left)
         area=Math.max(area, temp)
         if(height[left]>height[right])right--
         else left++
     }
-return area    
+   return area
 };
