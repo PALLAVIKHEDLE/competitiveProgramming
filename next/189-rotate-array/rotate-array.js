cea0786a87b6f2b71481c50e394ce5e0044cfb92
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-07-08 12:18:52 -0700

    Time: 88 ms (31.61%) | Memory: 57.2 MB (63.97%) - LeetSync

--- next/189-rotate-array/rotate-array.js
+++ next/189-rotate-array/rotate-array.js
@@ -8,14 +8,14 @@ var rotate = function(nums, k) {
     k=k%nums.length
     if(k==0)return
 
-const reverse=((start,end)=>{
-    while(start<=end){
-        [nums[end], nums[start]]=[nums[start],nums[end]]
-        start++
-        end--
-    }
-})
-reverse(0,nums.length-1)
-reverse(0, k-1)
-reverse(k, nums.length-1)
+    const reverse=((start,end)=>{
+        while(start<=end){
+            [nums[end], nums[start]]=[nums[start], nums[end]]
+            start++
+            end--
+        }
+    })
+    reverse(0, nums.length-1)
+    reverse(0, k-1)
+    reverse(k, nums.length-1)
 };
