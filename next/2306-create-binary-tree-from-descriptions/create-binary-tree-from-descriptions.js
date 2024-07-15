3b90844070edbe6720537f619a3290d222210f54
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-07-14 20:56:26 -0700

    Time: 444 ms (5.72%) | Memory: 77.2 MB (88.57%) - LeetSync

---
+++ next/2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.js
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[][]} descriptions
+ * @return {TreeNode}
+ */
+var createBinaryTree = function(descriptions) {
+    const map = {};
+    for (const description of descriptions) {
+        if (!(description[0] in map)) {
+            map[description[0]] = {
+                left: null,
+                right: null,
+                root: true
+            };
+        }
+        if (!(description[1] in map)) {
+            map[description[1]] = {
+                left: null,
+                right: null,
+                root: true
+            };
+        }
+
+        if (description[2]) map[description[0]].left = description[1];
+        else map[description[0]].right = description[1];
+
+        map[description[1]].root = false;
+    }
+
+    let root;
+    for (const key in map) {
+        if (map[key].root) {
+            root = new TreeNode(parseInt(key));
+            break;
+        }
+    }
+
+    const queue = [root];
+
+    while (queue.length > 0) {
+        const currentNode = queue.pop();
+        if (map[currentNode.val].left != null) {
+            currentNode.left = new TreeNode(map[currentNode.val].left);
+            queue.unshift(currentNode.left);
+        }
+        if (map[currentNode.val].right != null) {
+            currentNode.right = new TreeNode(map[currentNode.val].right);
+            queue.unshift(currentNode.right);
+        }
+    }
+
+    return root;
+};
