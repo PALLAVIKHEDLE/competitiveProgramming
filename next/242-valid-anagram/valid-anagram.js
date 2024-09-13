31257294c78cd1af81e9eb57c0360d150ef867b4
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-09-12 22:07:34 -0700

    Time: 62 ms (85.20%) | Memory: 53.8 MB (38.04%) - LeetSync

--- next/242-valid-anagram/valid-anagram.js
+++ next/242-valid-anagram/valid-anagram.js
@@ -5,12 +5,12 @@
  */
 var isAnagram = function(s, t) {
     let map={}
-     if (s.length !== t.length) return false; 
+    if(s.length!==t.length) return false;
 
     for(const ele of s)map[ele]=(map[ele]|0)+1
     for(const sec of t){
         if(map[sec])map[sec]--
         else return false
     }
-return true   
+    return true
 };
