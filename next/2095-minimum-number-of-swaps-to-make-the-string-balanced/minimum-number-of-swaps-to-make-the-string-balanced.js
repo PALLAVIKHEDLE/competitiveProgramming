f71a64c31facd1f3fe990038b06e74ba71ff589c
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-10-07 18:34:11 -0700

    Time: 109 ms (24.32%) | Memory: 79.8 MB (28.83%) - LeetSync

--- next/2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.js
+++ next/2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.js
@@ -1,12 +1,9 @@
-/**
- * @param {string} s
- * @return {number}
- */
 var minSwaps = function(s) {
-    let stack=[]
-    for(let i=0; i<s.length;i++){
-        if(s[i]=='[')stack.push(s[i])
-        else if(stack.length && s[i]==']')stack.pop(s[i])
+    let stack = [];
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] == '[') stack.push(s[i]);
+        else if (stack.length && s[i] == ']') stack.pop();
     }
-return stack.length/2
+    // Return the smallest number of swaps needed, rounding up to an integer
+    return Math.ceil(stack.length / 2);
 };
