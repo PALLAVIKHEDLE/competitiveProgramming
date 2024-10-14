b080bf1fe474fe0ccd8b1fed1c3133151cbbf0b4
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2024-10-13 17:43:00 -0700

    Time: 203 ms (92.00%) | Memory: 69 MB (100.00%) - LeetSync

---
+++ next/2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.js
@@ -0,0 +1,14 @@
+const maxKelements = function (nums, k) {
+  const pq = new MaxPriorityQueue({ compare: (a, b) => b - a })
+  for (const num of nums) {
+    pq.enqueue(num)
+  }
+  let score = 0
+  while (k) {
+    const ele = pq.dequeue()
+    score += ele
+    pq.enqueue(Math.ceil(ele / 3))
+    k--
+  }
+  return score
+}
