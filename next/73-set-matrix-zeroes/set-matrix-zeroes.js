7ac0308358d8c5c315e7bdf9015c52db4a62b116
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2025-05-21 16:38:47 -0700

    Time: 0 ms (100.00%) | Memory: 58.4 MB (88.72%) - LeetSync

--- next/73-set-matrix-zeroes/set-matrix-zeroes.js
+++ next/73-set-matrix-zeroes/set-matrix-zeroes.js
@@ -3,23 +3,30 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var setZeroes = function(matrix) {
-  const rows=matrix.length, cols=matrix[0].length;
-  let zeroRows=[],zeroCols=[]
-  for(let i=0; i<rows;i++){
-      for(let j=0;j<cols;j++){
+ let rows=matrix.length, cols=matrix[0].length,
+     zeroRows=[],zeroCols=[];
+
+     for(let i=0;i<rows;i++){
+         for(let j=0;j<cols;j++){
               //identify the rows and column containing zeros
-          if(matrix[i][j]==0){
-            zeroRows.push(i)
-            zeroCols.push(j)
-          }
-      }
-  }
-      // Set entire rows to 0
-  zeroRows.forEach(row=>{
-         for (let j = 0; j < cols; j++)matrix[row][j] = 0; 
-  })
-   // Set entire columns to 0
-   zeroCols.forEach((col)=>{
-        for (let i = 0; i < rows; i++)matrix[i][col] = 0;
-  })
+             if(matrix[i][j]==0){
+               zeroRows.push(i)  
+               zeroCols.push(j)  
+             }
+         }
+     }
+
+     //set Entire rows to zero
+     zeroRows.forEach((row)=>{
+         for(let j=0;j<cols;j++){
+             matrix[row][j]=0
+         }
+     })
+
+     //set entire col to zero
+     zeroCols.forEach((col)=>{
+         for(let j=0;j<rows;j++){
+             matrix[j][col]=0
+         }
+     })
 };
