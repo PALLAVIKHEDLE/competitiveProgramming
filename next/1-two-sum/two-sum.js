831cc957d5bb153fb183dfdc1bd4c90c5a6d5c17
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2025-09-20 12:00:56 -0700

    Time: 38 ms (20.93%) | Memory: 55.1 MB (53.17%) - LeetSync

--- next/1-two-sum/two-sum.js
+++ next/1-two-sum/two-sum.js
@@ -4,16 +4,22 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let map={}
-   for(let i=0; i<nums.length;i++){
-    let requiredNum=target-nums[i]
-    if(map[requiredNum]!==undefined)return [map[requiredNum],i]
-    map[nums[i]]=i
-   }
+//     let map={}
+//    for(let i=0; i<nums.length;i++){
+//     let requiredNum=target-nums[i]
+//     if(map[requiredNum]!==undefined)return [map[requiredNum],i]
+//     map[nums[i]]=i
+//    }
 //    for(let i=0; i<nums.length;i++){
 //     let requiredNum=target-nums[i]
 //     let index = nums.indexOf(requiredNum);
 //        if(index !== -1 && index !== i) return [i, index];
 //        else requiredNum=0
 //    }
+
+for(let i=0; i<=nums.length;i++){
+    for(let j=i+1; j<nums.length;j++){
+        if(target==nums[i]+nums[j])return[i,j]
+    }
+}
 };
