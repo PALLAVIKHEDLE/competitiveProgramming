19dc08b5bedb8fb2d0e35b8c8d8a0f4a5c39b1e2
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2025-09-20 11:38:23 -0700

    Time: 32 ms (27.56%) | Memory: 77.3 MB (18.34%) - LeetSync

--- next/217-contains-duplicate/contains-duplicate.js
+++ next/217-contains-duplicate/contains-duplicate.js
@@ -3,14 +3,20 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-   let map={}
-   for(let i=0; i<nums.length; i++){
-    let val=map[nums[i]]
-    if(val==undefined)map[nums[i]]=0
-    else map[nums[i]] = val+1;
-   }
-   for(let key in map){
-    if(map[key]>=1)return true
-   }
-   return false
+//    let map={}
+//    for(let i=0; i<nums.length; i++){
+//     let val=map[nums[i]]
+//     if(val==undefined)map[nums[i]]=0
+//     else map[nums[i]] = val+1;
+//    }
+//    for(let key in map){
+//     if(map[key]>=1)return true
+//    }
+//    return false
+let map={}
+for(let i=0; i<nums.length; i++){
+    if(map[nums[i]]==undefined)map[nums[i]]=0
+    else return true
+}
+return false
 };
