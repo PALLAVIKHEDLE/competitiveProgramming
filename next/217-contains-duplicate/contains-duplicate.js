4955711d66d0f1a7e2a331c89ee0dd92577665d4
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2025-09-20 11:22:18 -0700

    Time: 104 ms (5.19%) | Memory: 80.4 MB (5.21%) - LeetSync

--- next/217-contains-duplicate/contains-duplicate.js
+++ next/217-contains-duplicate/contains-duplicate.js
@@ -3,13 +3,14 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    let map={}
-    for(let i=0; i<nums.length; i++){
-        map[nums[i]]=(map[nums[i]]|0)+1
-    }
-    for(key in map){
-        console.log(map[key],key, 'map')
-        if(map[key]>1)return true
-    }
- return false   
+   let map={}
+   for(let i=0; i<nums.length; i++){
+    let val=map[nums[i]]
+    if(val==undefined)map[nums[i]]=0
+    else map[nums[i]] = val+1;
+   }
+   for(let key in map){
+    if(map[key]>=1)return true
+   }
+   return false
 };
