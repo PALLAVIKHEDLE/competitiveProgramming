748743a495d4687874d611304e0c617df94d6255
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2025-09-21 16:02:34 -0700

    Time: 85 ms (5.05%) | Memory: 64.5 MB (5.01%) - LeetSync

--- next/20-valid-parentheses/valid-parentheses.js
+++ next/20-valid-parentheses/valid-parentheses.js
@@ -3,13 +3,15 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    let openStack=[];
-    for(let i=0; i<s.length;i++){
-        let char=s[i], peekElement=openStack[openStack.length-1]
-        if(char=='('||char=='['|| char=='{')openStack.push(char)
-        else if(char==')' && peekElement=='(' || char=='}' && peekElement=='{' ||char==']' && peekElement=='[')openStack.pop();
+    let stack=[]
+    for(let i=0; i<s.length; i++){
+        let lastEle=stack[stack.length-1]
+        console.log(s[i])
+        if(s[i]=='('||s[i]=='['||s[i]=='{')stack.push(s[i])
+        else if(s[i]==')'&& lastEle=='('||s[i]==']'&&lastEle=='['||s[i]=='}'&&lastEle=='{')stack.pop()
         else return false
     }
-     if(openStack.length==0)return true
-     else return false
+    if(stack.length==0)return true
+
+return false
 };
