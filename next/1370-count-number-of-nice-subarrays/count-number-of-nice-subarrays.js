2d86ef56d2addec8862e53919a4cdbcf76322801
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2025-09-23 14:44:40 -0700

    Time: 5 ms (98.88%) | Memory: 64.8 MB (62.47%) - LeetSync

--- next/1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.js
+++ next/1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.js
@@ -4,18 +4,18 @@
  * @return {number}
  */
 var numberOfSubarrays = function(nums, k) {
-  let odd = 0, start = 0, count = 0, prefix = 0;
-    for(let i = 0; i < nums.length; i++){
-        if(nums[i] % 2 !== 0){
-            odd++;
-            prefix = 0;
+    let start=0, odd=0, count=0, prefix=0;
+    for(let i=0; i<nums.length; i++){
+        if(nums[i]%2!==0){
+            odd++
+            prefix=0   // Reset prefix (new odd resets subarray count)
         }
-        while(odd == k && start <= i){
-            if(nums[start] % 2 !== 0) odd--;
-            start++;
-            prefix++;
+        while(odd==k &&start<=i){
+            if(nums[start]%2!==0)odd--;
+            start++
+            prefix++
         }
-        count += prefix;
+        count+=prefix
     }
-    return count; 
+    return count
 };
