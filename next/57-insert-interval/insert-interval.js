e1a9b1e71cb69c0c663c84aecf9cb8330b1e02cd
Pallavi khedle <40143360+PALLAVIKHEDLE@users.noreply.github.com>
2025-10-07 23:13:50 -0700

    Time: 2 ms (83.16%) | Memory: 58.8 MB (46.12%) - LeetSync

--- next/57-insert-interval/insert-interval.js
+++ next/57-insert-interval/insert-interval.js
@@ -4,18 +4,23 @@
  * @return {number[][]}
  */
 var insert = function(intervals, newInterval) {
-  let result=[], i;
-
-  for(i=0; i<intervals.length; i++){
-      if(intervals[i][1]<newInterval[0])result.push(intervals[i])
-      else if(intervals[i][0]>newInterval[1])break
-      else{
-          newInterval[0]=Math.min(intervals[i][0], newInterval[0])
-          newInterval[1]=Math.max(intervals[i][1], newInterval[1])
-      }
-  }
-  result.push(newInterval)
-  for(i; i<intervals.length;i++) result.push(intervals[i])
-
-return result  
+    let result=[], i
+    for(i=0; i<intervals.length;i++){
+        // 1. Interval ends before newInterval starts → push as is
+        if(intervals[i][1]<newInterval[0])result.push(intervals[i])
+        // 2. Interval starts after newInterval ends → stop merging
+        else if(intervals[i][0]>newInterval[1])break
+         // 3. Overlapping → merge
+        else{
+            newInterval[0]=Math.min(intervals[i][0], newInterval[0])
+            newInterval[1]=Math.max(intervals[i][1], newInterval[1])
+        }
+    }
+    //push merged new interval
+    result.push(newInterval)
+      // Push the rest of the intervals
+ for(i; i<intervals.length; i++)result.push(intervals[i])
+return result
 };
+// [1,3][2,5][6,9]
+// [1,5][6,9]
